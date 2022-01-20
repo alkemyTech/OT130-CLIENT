f6187f9e1258e276f64d0f1a56d8600fd4e0fc8a
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-20 11:59:30 -0300

    adding validation inputs

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import {Formik} from 'formik';
 import '../FormStyles.css';
 
@@ -35,18 +34,36 @@ const RegisterForm = () => {
     return(
         <>
             <Formik
-                onSubmit={()=>{
-                    console.log('Formulario enviado')
+                initialValues={{
+                    name: '',
+                    lastName: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+                }}
+                //Validation 
+                validate={(values) => {
+                    let error = {};
+                    if(!values.name){
+                        error.name = 'Name is required';
+                    }
+                }}
+                onSubmit={(values)=>{
+                    console.log(values);
+                    console.log('submit');
                 }}
             >
-                {()=>(
-                    <form className="form-container" /*  onSubmit={handleSubmit} */>
-                        <input className="input-field" type="text" name="name"/*  value={initialValues.name}  onChange={handleChange}*/ placeholder="Enter name"></input>
-                        <input className="input-field" type="text" name="lastName" /* value={initialValues.lastName} onChange={handleChange} */ placeholder="Enter last name"></input>
+                {({values,errors,touched,handleSubmit,handleChange,handleBlur})=>(
+                    <form className="form-container"  onSubmit={handleSubmit}>
+                        <input className="input-field" type="text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} placeholder="Enter name"></input>
+                        {errors.name && touched.name && errors.name}
+                        <input className="input-field" type="text" name="lastName" value={values.lastName} onChange={handleChange} onBlur={handleBlur} placeholder="Enter last name"></input>
+                        <input className="input-field" type="text" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} placeholder="Email"></input>
+                        <input className="input-field" type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} placeholder="Password"></input>
+                        <input className="input-field" type="password" name="confirmPassword" value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur} placeholder="Confirm password"></input>
                         <button className="submit-btn" type="submit">Register</button>
                     </form>
                 )}
-
             </Formik>
         </>
     )
