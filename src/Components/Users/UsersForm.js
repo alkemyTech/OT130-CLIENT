215469b60f7a531e43e936993a58165d867a615c
Mateo <mateolewinzon@gmail.com>
2022-01-20 19:46:26 -0300

    Reordering code, following Gonzalo's recommendations

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -16,100 +16,101 @@ const validation = Yup.object().shape({
   Role: Yup.string().required("Campo obligatorio"),
   Image: Yup.mixed().test(
     "fileType",
-    "Suba una imagen con extensión .jpg o .png",
+    "Extensión inválida. Solo archivos jpg o png",
     (value) => {
-      if (value) return ["image/jpeg", "image/png"].includes(value.type); 
-      //Assuming image is not required 
-      else return true
+      if (value) return ["image/jpeg", "image/png"].includes(value.type);
+      //Assuming image is not required
+      else return true;
     }
   ),
 });
+
+const initialValues = {
+  Name: "",
+  Email: "",
+  Description: "",
+  Role: "usuario",
+  Image: null,
+};
+
+const submit = (values, setSubmitting, user) => {
+  if (user) {
+    // do some
+  } else {
+    // do some
+  }
+};
+
+const FormComponent = ({ touched, handleSubmit, setFieldValue }) => {
+  return <Form onSubmit={handleSubmit} className="form-container">
+    <Field
+      className="input-field"
+      type="text"
+      name="Name"
+      placeholder="Nombre"
+    ></Field>
+
+    {touched.Name ? <ErrorMessage name="Name" /> : null}
+
+    <Field
+      className="input-field"
+      type="text"
+      name="Email"
+      placeholder="Email"
+    ></Field>
+
+    {touched.Email ? <ErrorMessage name="Email" /> : null}
+
+    <Field
+      className="input-field"
+      type="text"
+      name="Description"
+      placeholder="Descripción"
+    ></Field>
+
+    {touched.Description ? <ErrorMessage name="Description" /> : null}
+
+    <Field className="select-field" as="select" name="Role">
+      <option value="usuario">Usuario</option>
+      <option value="administrador">Administrador</option>
+    </Field>
+    <div>
+      <label style={{ paddingRight: "10px" }}>Subir imagen de usuario</label>
+      <input
+        type="file"
+        max={1}
+        name="Image"
+        accept="image/png, image/jpeg"
+        onChange={(e) => {
+          setFieldValue("Image", e.currentTarget.files[0]);
+        }}
+      />
+      <div>
+        <ErrorMessage name="Image" />
+      </div>
+    </div>
+    <button type="submit" disabled={false} className="submit-btn">
+      Enviar
+    </button>
+  </Form>;
+};
 
 const UserForm = ({ user }) => {
   return (
     <Formik
       validationSchema={validation}
-      initialValues={
-        user || {
-          Name: "",
-          Email: "",
-          Description: "",
-          Role: "usuario",
-          Image: null,
-        }
+      initialValues={user || initialValues}
+      onSubmit={(values, { setSubmitting }) =>
+        submit(values, setSubmitting, user)
       }
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        if (user) {
-          // post and handle
-        } else {
-          // update and handle 
-        }
-      }}
     >
-      {({
-        handleSubmit,
-        isSubmitting,
-        validateField,
-        errors,
-        isValid,
-        setFieldValue,
-        touched,
-      }) => {
+      {({ handleSubmit, setFieldValue, touched }) => {
         return (
-          <Form onSubmit={handleSubmit} className="form-container">
-            <Field
-              className="input-field"
-              type="text"
-              name="Name"
-              placeholder="Nombre"
-            ></Field>
-
-            {touched.Name ? <ErrorMessage name="Name" /> : null}
-
-            <Field
-              className="input-field"
-              type="text"
-              name="Email"
-              placeholder="Email"
-            ></Field>
-
-            {touched.Email ? <ErrorMessage name="Email" /> : null}
-
-            <Field
-              className="input-field"
-              type="text"
-              name="Description"
-              placeholder="Descripción"
-            ></Field>
-
-            {touched.Description ? <ErrorMessage name="Description" /> : null}
-
-            <Field className="select-field" as="select" name="Role">
-              <option value="usuario">Usuario</option>
-              <option value="administrador">Administrador</option>
-            </Field>
-            <div>
-              <label style={{ paddingRight: "10px" }}>
-                Subir imagen de usuario
-              </label>
-              <input
-                type="file"
-                max={1}
-                name="Image"
-                accept="image/png, image/jpeg"
-                onChange={(e) => {
-                  setFieldValue("Image", e.currentTarget.files[0]);
-                }}
-              />
-              <div>
-                <ErrorMessage name="Image" />
-              </div>
-            </div>
-            <button type="submit" className="submit-btn">
-              Enviar
-            </button>
-          </Form>
+          <FormComponent
+            touched={touched}
+            handleSubmit={handleSubmit}
+            setFieldValue={setFieldValue}
+          />
         );
       }}
     </Formik>
