6c9c32c127dbc178c9eaaa425e0c2035271ecb96
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-20 13:10:00 -0300

    completed registration form

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,37 +1,10 @@
 import React , {useState} from 'react';
-import {Formik} from 'formik';
+import {Formik, Form, Field, ErrorMessage } from 'formik';
 import '../FormStyles.css';
 
 const RegisterForm = () => {
     const [submitForm, setSubmitForm] = useState(false);
-    /* Comienzo 11:03 */
-
-/*     const [initialValues, setInitialValues] = useState({
-        name: '',
-        lastName: ''
-    })
     
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'lastName'){
-            setInitialValues({...initialValues, lastName: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
-
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Enter name"></input>
-            <input className="input-field" type="text" name="lastName" value={initialValues.lastName} onChange={handleChange} placeholder="Enter last name"></input>
-            <button className="submit-btn" type="submit">Register</button>
-        </form>
-    ); */
     return(
         <>
             <Formik
@@ -81,21 +54,21 @@ const RegisterForm = () => {
                     console.log('Form submit');
                 }}
             >
-                {({values,errors,touched,handleSubmit,handleChange,handleBlur})=>(
-                    <form className="form-container"  onSubmit={handleSubmit}>
-                        <input className="input-field" type="text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} placeholder="Enter name"></input>
-                        {errors.name && touched.name && errors.name ? (<div className="error-message">{errors.name}</div>) : null}
-                        <input className="input-field" type="text" name="lastName" value={values.lastName} onChange={handleChange} onBlur={handleBlur} placeholder="Enter last name"></input>
-                        {errors.lastName && touched.lastName && errors.lastName ? (<div className="error-message">{errors.lastName}</div>) : null}
-                        <input className="input-field" type="text" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} placeholder="Email"></input>
-                        {errors.email && touched.email && errors.email ? (<div className="error-message">{errors.email}</div>) : null}
-                        <input className="input-field" type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} placeholder="Password"></input>
-                        {errors.password && touched.password && errors.password ? (<div className="error-message">{errors.password}</div>) : null}
-                        <input className="input-field" type="password" name="confirmPassword" value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur} placeholder="Confirm password"></input>
-                        {errors.confirmPassword && touched.confirmPassword && errors.confirmPassword ? (<div className="error-message">{errors.confirmPassword}</div>) : null}
+                {({errors})=>(
+                    <Form className="form-container">
+                        <Field className="input-field" type="text" name="name"  placeholder="Enter name"></Field>
+                        <ErrorMessage name="name" component={()=>(<div className='error-message'>{errors.name}</div>)}/>   
+                        <Field className="input-field" type="text" name="lastName" placeholder="Enter last name"></Field>
+                        <ErrorMessage name="lastName" component={()=>(<div className='error-message'>{errors.lastName}</div>)}/> 
+                        <Field className="input-field" type="text" name="email" placeholder="Email"></Field>
+                        <ErrorMessage name="email" component={()=>(<div className='error-message'>{errors.email}</div>)}/> 
+                        <Field className="input-field" type="password" name="password" placeholder="Password"></Field>
+                        <ErrorMessage name="password" component={()=>(<div className='error-message'>{errors.password}</div>)}/> 
+                        <Field className="input-field" type="password" name="confirmPassword" placeholder="Confirm password"></Field>
+                        <ErrorMessage name="confirmPassword" component={()=>(<div className='error-message'>{errors.confirmPassword}</div>)}/> 
                         <button className="submit-btn" type="submit">Register</button>
                         {submitForm && <div className="success-message">Form submitted successfully</div>}
-                    </form>
+                    </Form>
                 )}
             </Formik>
         </>
