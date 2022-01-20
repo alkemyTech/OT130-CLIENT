49ea8f54248cb79d066bdf63e49db51428c71e04
IgnacioVivas <ignacab@live.com>
2022-01-20 11:38:57 -0300

    CKEditor library incorporation

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import '../FormStyles.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const SlidesForm = () => {
     const [initialValues, setInitialValues] = useState({
         name: '',
-        description: ''
+        description: '',
+        image: ''
     });
 
     const handleChange = (e) => {
+        console.log(e);
         if(e.target.name === 'name'){
             setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
+        } else if(e.target.name === 'image'){
+            setInitialValues({...initialValues, image: e.target.value})
         }
+    }
+
+    const handleChangeDescription = (event, editor) => {
+        const data = editor.getData();
+        setInitialValues({...initialValues, description: data})
     }
 
     const handleSubmit = (e) =>{
@@ -23,7 +32,8 @@ const SlidesForm = () => {
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
+            <CKEditor className="input-field" type="text" name="description" editor = { ClassicEditor } value={initialValues.description} onChange={handleChangeDescription} placeholder="Write the description"/>
+            <input className='input-field' type="file" name='image' accept="image/png, image/jpeg" placeholder='Add image' value={initialValues.image} onChange={handleChange}></input>
             <button className="submit-btn" type="submit">Send</button>
         </form>
     );
