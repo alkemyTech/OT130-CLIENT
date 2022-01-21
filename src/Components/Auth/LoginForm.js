8780db76daae480fdd14f38d5ebc826d546f4d04
Rodrigo <rodrigomendozar10@gmail.com>
2022-01-21 13:19:08 -0300

    instalación bootstrap/MUI/Yup para formLogin. Creación de componente LoginForm.js y loginForm.css

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,33 +1,85 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React from "react";
+import "../FormStyles.css";
+// Import Formik
+import { useFormik } from "formik";
+// Import Yup
+import * as yup from "yup";
+// Import material UI.
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { Container } from "react-bootstrap";
+// import css
+import "./loginForm.css";
 
 const LoginForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        email: '',
-        password: ''
-    });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        } if(e.target.name === 'password'){
-            setInitialValues({...initialValues, password: e.target.value})
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
+  const validationSchema = yup.object({
+    email: yup
+      .string("Ingrese su email")
+      .email("Ingrese un email valido")
+      .required("Email requerido"),
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-            <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-            <button className="submit-btn" type="submit">Log In</button>
-        </form>
-    );
-}
- 
+    password: yup
+      .string("Ingrese su contraseña")
+      .min(6, "La contraseña debe tener como minimo 6 caracteres")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
+        "La contraseña debe tener una longitud mínima de 6 caracteres, y contener al menos un número, una letra y un símbolo (por ejemplo: @#$%)."
+      )
+      .required("Contraseña requerida"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+        // console.log(values);
+    },
+  });
+
+  return (
+            <Container>
+                <form
+                    className="form-login d-flex flex-column "
+                    onSubmit={formik.handleSubmit}
+                >
+                        <TextField
+                            className="input-login"
+                            fullWidth
+                            id="email"
+                            name="email"
+                            label="Email"
+                            maxLength="35"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email}
+                        />
+                        <TextField
+                            className="input-login"
+                            fullWidth
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            maxLength="15"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            error={formik.touched.password && Boolean(formik.errors.password)}
+                            helperText={formik.touched.password && formik.errors.password}
+                        />
+                        <Button 
+                            variant="contained"
+                            className="form-button" 
+                            type="submit">
+                                Log In
+                        </Button>
+                </form>
+            </Container>
+  );
+};
+
 export default LoginForm;
