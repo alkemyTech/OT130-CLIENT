45819cb9d40693a118acb72643131439673c8967
Shonakar <carlosjbordon@gmail.com>
2022-01-20 21:57:21 -0300

    Validations, partch and post

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -2,33 +2,78 @@ import React, {useState} from "react";
 import {CKEditor} from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "../../Components/FormStyles.css";
+import axios from "axios";
 
-const NewsForm = () => {
-  const [initialValues, setInitialValues] = useState({
+const NewsForm = ({news}) => {
+  const iVal = news || {
+    id: "",
     title: "",
     content: "",
+    image: "",
     category: "",
-  });
+  };
+
+  const [initialValues, setInitialValues] = useState(iVal);
+  const [error, setError] = useState(false);
 
   const handleChange = (e) => {
     if (e.target.name === "title") {
       setInitialValues({...initialValues, title: e.target.value});
     }
-    if (e.target.name === "content") {
-      setInitialValues({...initialValues, content: e.target.value});
-    }
     if (e.target.name === "category") {
       setInitialValues({...initialValues, category: e.target.value});
     }
+    if (e.target.name === "img") {
+      console.log(e.target.files[0].name);
+      setInitialValues({...initialValues, image: e.target.files[0].name});
+    }
+    console.log(initialValues);
+  };
+
+  const handleChangeCKE = (event, editor) => {
+    const data = editor.getData();
+
+    setInitialValues({...initialValues, content: data});
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const {title, content, category} = initialValues;
+
+    if ([title, content, category].includes("")) {
+      setError(true);
+      return;
+    } else if (title.length < 4) {
+      alert("El titulo debe tener minimo 4 caracteres");
+      return;
+    }
+
+    setError(false);
+
+    if (initialValues.id) {
+      axios.patch(
+        `http://ongapi.alkemy.org/api/news/${initialValues.id}`,
+        initialValues
+      );
+      console.log("patch");
+    } else {
+      axios.post("http://ongapi.alkemy.org/api/news", initialValues);
+      console.log("post");
+    }
+
+    setInitialValues({id: "", title: "", content: "", image: "", category: ""});
+
     console.log(initialValues);
   };
 
   return (
     <form className="form-container" onSubmit={handleSubmit}>
+      {error && (
+        <div className="">
+          <p>Todos los campos son obligatorios</p>
+        </div>
+      )}
       <input
         className="input-field"
         type="text"
@@ -40,10 +85,17 @@ const NewsForm = () => {
 
       <CKEditor
         editor={ClassicEditor}
-        data="<p>Aca algo de data para mostrar</p>"
+        data={initialValues.content || ""}
+        onChange={handleChangeCKE}
       />
 
-      <input className="input-field" type="file" accept="image/*" />
+      <input
+        className="select-field"
+        type="file"
+        name="img"
+        accept="image/*"
+        onChange={handleChange}
+      />
       <select
         className="select-field"
         name="category"
@@ -58,7 +110,7 @@ const NewsForm = () => {
         <option value="3">Demo option 3</option>
       </select>
       <button className="submit-btn" type="submit">
-        Send
+        {initialValues.id ? "Edit" : "Send"}
       </button>
     </form>
   );
