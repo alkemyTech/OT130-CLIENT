1ca686e9d94fd7b2bd4f6089bd231e71002927c0
franco646 <franco@>
2022-01-24 10:43:50 -0300

    Update getOrganizationData.js

--- src/Services/getOrganizationData.js
+++ src/Services/getOrganizationData.js
@@ -1,12 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getOrganizationData = async () => {
-    try {
-        const response = await axios.get('http://ongapi.alkemy.org/api/organization');
-        return response.data
-    } catch (error) {
-        console.log(error)
-    }
-}
+  const response = await axios.get("http://ongapi.alkemy.org/api/organization");
+  return response.data;
+};
 
 export default getOrganizationData;
