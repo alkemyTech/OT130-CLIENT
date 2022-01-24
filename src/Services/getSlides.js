11ae6601399316374a072f433101d4bafc3693b4
franco646 <franco@>
2022-01-24 10:43:55 -0300

    Update getSlides.js

--- src/Services/getSlides.js
+++ src/Services/getSlides.js
@@ -1,12 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getSlides = async () => {
-    try {
-        const response = await axios.get("http://ongapi.alkemy.org/api/slides");
-        return response.data
-    } catch (error) {
-        console.log(error)
-    }
-}
+  const response = await axios.get("http://ongapi.alkemy.org/api/slides");
+  return response.data;
+};
 
 export default getSlides;
