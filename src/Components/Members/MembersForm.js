6a620dddc9c4beddecdac82c729362ee2f5f08c5
Rodrigo <rodrigomendozar10@gmail.com>
2022-01-24 16:34:49 -0300

    files input

--- src/Components/Members/MembersForm.js
+++ src/Components/Members/MembersForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import '../FormStyles.css';
 
 const MembersForm = () => {
@@ -23,8 +24,12 @@ const MembersForm = () => {
   return (
     <form className="form-container" onSubmit={handleSubmit}>
       <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
+      {/* <input className="input-field" type="image" name="image" value={initialValues.name} onChange={handleChange} placeholder="Name"></input> */}
       <textarea className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></textarea>
-      <input className="input-field w-50" type="url" name="social-media-link" value={initialValues.name} onChange={handleChange} placeholder="Social Media Link"></input>
+      <div className="d-flex  justify-content-between mx-0 p-0">
+      <input className="input-field w-50 me-1 " type="url" name="social-media-link" value={initialValues.name} onChange={handleChange} placeholder="Linkedin"></input>
+      <input className="input-field w-50 " type="url" name="social-media-link" value={initialValues.name} onChange={handleChange} placeholder="Facebook"></input>
+      </div>
       <button className="submit-btn" type="submit">Send</button>
     </form>
   );
