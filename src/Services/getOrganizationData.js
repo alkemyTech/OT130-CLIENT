8954622b00fbc1c127f846c86e356647de55a4db
franco646 <franco@>
2022-01-25 16:35:14 -0300

    Delete getOrganizationData.js

--- src/Services/getOrganizationData.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const getOrganizationData = async () => {
-  const response = await axios.get("http://ongapi.alkemy.org/api/organization");
-  return response.data;
-};
-
-export default getOrganizationData;
