84bb6c38880bb26d54d08af5849b8b0f85d41b32
Shonakar <carlosjbordon@gmail.com>
2022-01-25 14:03:17 -0300

    implement formik

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import {Field, Formik} from "formik";
 import "../FormStyles.css";
 const emptyContact = {
   name: "",
@@ -8,67 +8,59 @@ const emptyContact = {
 };
 
 const ContactForm = () => {
-  const [contact, setContact] = useState(emptyContact);
-
-  const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setContact({...contact, name: e.target.value});
-    }
-    if (e.target.name === "email") {
-      setContact({...contact, email: e.target.value});
-    }
-    if (e.target.name === "phone") {
-      setContact({...contact, phone: e.target.value});
-    }
-    if (e.target.name === "message") {
-      setContact({...contact, message: e.target.value});
-    }
-    console.log(contact);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const {name, email, phone, message} = contact;
-
-    //Check Errors
-
-    //sendForm
-  };
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="select-field"
-        type="text"
-        name="name"
-        onChange={handleChange}
-        placeholder="Name"
-      />
-      <input
-        className="select-field"
-        type="email"
-        name="email"
-        onChange={handleChange}
-        placeholder="Email"
-      />
-      <input
-        className="select-field"
-        type="tel"
-        name="phone"
-        onChange={handleChange}
-        placeholder="Phone"
-      />
-      <textarea
-        className="select-field"
-        name="message"
-        cols="30"
-        rows="10"
-        style={{resize: "none"}}
-        onChange={handleChange}
-      />
-      <button type="submit" className="submit-btn">
-        Enviar
-      </button>
-    </form>
+    <Formik
+      initialValues={emptyContact}
+      onSubmit={(val, {resetForm}) => {
+        resetForm();
+
+        console.log({
+          name: val.name,
+          email: val.email,
+          phone: val.phone,
+          message: val.message,
+        });
+      }}
+      validate={(val) => {}}
+    >
+      {({values, errors, touched, handleSubmit, handleChange, handleBlur}) => (
+        <form className="form-container" onSubmit={handleSubmit}>
+          <Field
+            className="select-field"
+            type="text"
+            name="name"
+            onChange={handleChange("name")}
+            placeholder="Name"
+          />
+          <Field
+            className="select-field"
+            type="email"
+            name="email"
+            onChange={handleChange("email")}
+            placeholder="Email"
+          />
+          <Field
+            className="select-field"
+            type="tel"
+            name="phone"
+            onChange={handleChange("phone")}
+            placeholder="Phone"
+          />
+          <Field
+            component="textarea"
+            className="select-field"
+            name="message"
+            cols="30"
+            rows="10"
+            style={{resize: "none"}}
+            onChange={handleChange("message")}
+          />
+          <button type="submit" className="submit-btn">
+            Enviar
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
