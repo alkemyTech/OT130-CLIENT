ad83a03d35ed5ca1a76cbb7208ca687f9bad0e53
Mateo <mateolewinzon@gmail.com>
2022-01-24 23:57:40 -0300

    Fixed privateApiService.js

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -14,6 +14,6 @@ const Post = async (url, data) => await instance.post(url, data);
 
 const Patch = async (url, data) => await instance.patch(url, data);
 
-const Get = async (url) => await instance.post(url);
+const Get = async (url) => await instance.get(url);
 
 export { Post, Patch, Get };
