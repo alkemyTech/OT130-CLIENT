bf23c8b5d83ba0e3d5cc099f064a416cd83de326
--local <xbynachox@gmail.com>
2022-01-25 11:36:23 -0300

    moved API to new file

---
+++ src/Services/organiationService.js
@@ -0,0 +1,19 @@
+import { HOST } from "./config";
+
+export const getOrganizationData = async () => {
+  try {
+    const response = await axios.get(`${HOST}/organization`);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const updateOrganizationData = async (body, id) => {
+  try {
+    const response = await axios.put(`${HOST}/organization/${id}`, body);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
