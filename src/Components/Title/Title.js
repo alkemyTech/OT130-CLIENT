7b4b7c5bf49d2003a3294d208bef5f86a183cfdc
Mateo <mateolewinzon@gmail.com>
2022-01-25 12:12:03 -0300

    PR fixes

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Row, Container } from "react-bootstrap";
-import "./Title.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
+import "./title.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Title = ({ text, image }) => {
   return (
-    <Container style={{backgroundImage:`url(${image||"images/banner-img.jpg"})`}} fluid className="title-container vertical-center">
-      <Row className="justify-content-center">
+    <Container className="title-container" fluid>
+        <img className="title-image"  src={image || "images/banner-img.jpg"} />
         <p className="title-text">{text}</p>
-      </Row>
     </Container>
   );
 };
