9bcb7905fac920d60559f87da1c051bfb623c06f
franco646 <franco@>
2022-01-25 16:35:30 -0300

    Delete postOrganizationData.js

--- src/Services/postOrganizationData.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const postOrganizationData = async (data) => {
-  await axios.post("http://ongapi.alkemy.org/api/organization", data);
-};
-
-export default postOrganizationData;
