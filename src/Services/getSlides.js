8a2acd58067a54a3a91e8b1b70bed23507184eb2
franco646 <franco@>
2022-01-25 16:35:19 -0300

    Delete getSlides.js

--- src/Services/getSlides.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const getSlides = async () => {
-  const response = await axios.get("http://ongapi.alkemy.org/api/slides");
-  return response.data;
-};
-
-export default getSlides;
