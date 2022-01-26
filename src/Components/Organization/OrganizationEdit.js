dcfba256bdb93424fefd908b1de4bdff5f59bfac
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-26 16:52:49 -0300

    adding validation inputs and CKEditor on formedit

--- src/Components/Organization/OrganizationEdit.js
+++ src/Components/Organization/OrganizationEdit.js
@@ -1,17 +1,15 @@
-import { useFormik  } from "formik";
+import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
-import { Redirect, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Redirect, useHistory, Link } from "react-router-dom/cjs/react-router-dom.min";
 import * as Yup from "yup";
-import { updateOrganizationData , getByIdOrganizationData } from "../../Services/organiationService";
-import { yupShortDesc, yupTitles , yupImages , yupLongDesc , yupUrlWebSite} from "../../Helpers/formValidations";
-import {CKEditor} from "@ckeditor/ckeditor5-react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import "./style.css";
-import { LOGO } from "../../assets";
-import { ORGANIZATION } from "../../rutas/config";
+
+import { updateOrganizationData } from "../../Services/organiationService";
+import { yupShortDesc, yupTitles , yupImages , yupLongDesc , yupUrlWebSite} from "../../Helpers/formValidations";
 import { toBase64 } from "../../Helpers/base64";
-
-
+import "./style.css";
+import { ORGANIZATION } from "../../rutas/config";
 
 
 const OrganizationEdit = () => {
@@ -20,32 +18,31 @@ const OrganizationEdit = () => {
     location: { state },
   } = useHistory();
   const id = state?.id || null;
-/*   const [selectedImage, setSelectedImage] = useState({ image: LOGO }); */
-  const [organization, setOrganization] = useState({});
+  const [submitForm, setSubmitForm] = useState(false);
 
-
-  useEffect(() => {
-    handleGetByIdOrganization();
-  },[]);
+  const timerMessage = (time) => {
+    setTimeout(() => {
+        setSubmitForm(false);
+        push(ORGANIZATION);
+    }, time);
+  };
 
   const handleCKEditorChange = (editor, setFieldValue) => {
-    formik.setFieldValue("short_description", editor.getData());
+    setFieldValue("short_description", editor.getData());
   };
-  const handleGetByIdOrganization = async () => {
-    const { data } = await getByIdOrganizationData(id);
-    setOrganization(data);
-  };
+
   const validationSchema = Yup.object().shape({
     name: yupTitles(),
-    short_description: yupShortDesc(),
     long_description: yupLongDesc(),
-    /* logo: yupImages(), */
+    short_description: yupShortDesc(),
+    logo: yupImages(),
     linkedin_url: yupUrlWebSite(),
     instagram_url: yupUrlWebSite(),
     facebook_url: yupUrlWebSite(),
     twitter_url: yupUrlWebSite() 
   });
-  const initialValues = {
+
+  const initialValues =  {
     name: '',
     long_description: '',
     short_description: '',
@@ -55,13 +52,6 @@ const OrganizationEdit = () => {
     twitter_url : '',
     logo: ''
   };
-  const toBase64 = (file) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
 
   const onSubmit = async (values) => {
     try{
@@ -69,264 +59,170 @@ const OrganizationEdit = () => {
     }catch(error){
       console.log(error);
     }
-
     await updateOrganizationData(values, id);
-    console.log('Form data' , values)
-    console.log(values.logo, 'logo')
+    setSubmitForm(true);
+    timerMessage(3000);
     formik.resetForm();
-    push(ORGANIZATION);
+
   };
+ 
   const formik = useFormik({
     initialValues,
-
     onSubmit,
-    // validate,
     validationSchema
   })
-  
-  
 
-  return id?
- 
-   (
-    <div className="container">
-
-      <div className="organization-fields-container">
-        <h1>Editar Organización</h1>
-        <form  onSubmit={formik.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Nombre</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              placeholder="Nombre"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.name && formik.errors.name ? (
-              <div className="alert alert-danger">{formik.errors.name}</div>
-            ) : null}
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="long_description">Descripción Larga</label>
-            <input
-              type="text"
-              className="form-control"
-              id="long_description"
-              name="long_description"
-              placeholder="Descripción Larga"
-              value={formik.values.long_description}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.long_description && formik.errors.long_description ? (
-              <div className="alert alert-danger">{formik.errors.long_description}</div>
-            ) : null}
-          </div>
-          <CKEditor
-        editor={ClassicEditor}
-        data={initialValues?.short_description}
-        onChange={(e, editor) => handleCKEditorChange(editor, formik.setFieldValue)}
-    />
+  return id
+  ?
+    (
+      <div className="container ">
+        <div className="organization-fields-container row">
+          <h1>Editar Organización</h1>
+          <form  onSubmit={formik.handleSubmit}>
         
-        {formik.touched.long_description && formik.errors.short_description &&
-              <div className="alert alert-danger">{formik.errors.short_description}</div>}
+            <div className="form-group mb-4 mt-4">
+              <label htmlFor="name">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder="Nombre"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.name && formik.errors.name &&
+                <div className="error-message message">{formik.errors.name}</div>
+              }
+            </div>
 
-        
-          <div className="form-group">
-            <label htmlFor="linkedin_url">Linkedin</label>
-            <input
-              type="text"
-              className="form-control"
-              id="linkedin_url"
-              name="linkedin_url"
-              placeholder="Linkedin"
-              value={formik.values.linkedin_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.linkedin_url && formik.errors.linkedin_url ? (
-              <div className="alert alert-danger">{formik.errors.linkedin_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="instagram_url">Instagram</label>
-            <input
-              type="text"
-              className="form-control"
-              id="instagram_url"
-              name="instagram_url"
-              placeholder="Instagram"
-              value={formik.values?.instagram_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}  
-            />
-            {formik.touched.instagram_url && formik.errors.instagram_url ? (
-              <div className="alert alert-danger">{formik.errors.instagram_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="facebook_url">Facebook</label>
-            <input
-              type="text"
-              className="form-control"
-              id="facebook_url"
-              name="facebook_url"
-              placeholder="Facebook"
-              value={formik.values.facebook_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.facebook_url && formik.errors.facebook_url ? (
-              <div className="alert alert-danger">{formik.errors.facebook_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="twitter_url">Twitter</label>
-            <input
-              type="text"
-              className="form-control"
-              id="twitter_url"
-              name="twitter_url"
-              placeholder="Twitter"
-              value={formik.values.twitter_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.twitter_url && formik.errors.twitter_url ? (
-              <div className="alert alert-danger">{formik.errors.twitter_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="logo">Logo</label>
-            <input
-              ref={formik.fileInput}
-              type="file"
-              className="form-control"
-              id="logo"
-              name="logo"
-              placeholder="Logo"
-              onChange={(e) => {
-                formik.setFieldValue('logo', e.currentTarget.files[0]);
-              }}
-            />
-            {formik.touched.logo && formik.errors.logo ? (
-              <div className="alert alert-danger">{formik.errors.logo}</div>
-            ) : null}
-          </div>
-{/*          
-          <div className="form-group">
-            <label htmlFor="long_description">Descripción Larga</label>
-            <input
-              type="text"
-              className="form-control"
-              id="long_description"
-              name="long_description"
-              placeholder="Descripción Larga"
-              value={formik.values.long_description}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.long_description && formik.errors.long_description ? (
-              <div className="alert alert-danger">{formik.errors.long_description}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="logo">Logo</label>
-            <input
-              type="file"
-              className="form-control"
-              max={1}
-              accept="image/jpeg, image/png"
-              id="logo"
-              name="logo"
-              placeholder="Logo"
-              value={formik.values.logo}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.logo && formik.errors.logo ? (
-              <div className="alert alert-danger">{formik.errors.logo}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="linkedin_url">Linkedin</label>
-            <input
-              type="text"
-              className="form-control"
-              id="linkedin_url"
-              name="linkedin_url"
-              placeholder="Linkedin"
-              value={formik.values.linkedin_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.linkedin_url && formik.errors.linkedin_url ? (
-              <div className="alert alert-danger">{formik.errors.linkedin_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="instagram_url">Instagram</label>
-            <input
-              type="text"
-              className="form-control"
-              id="instagram_url"
-              name="instagram_url"
-              placeholder="Instagram"
-              value={formik.values?.instagram_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}  
-            />
-            {formik.touched.instagram_url && formik.errors.instagram_url ? (
-              <div className="alert alert-danger">{formik.errors.instagram_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="facebook_url">Facebook</label>
-            <input
-              type="text"
-              className="form-control"
-              id="facebook_url"
-              name="facebook_url"
-              placeholder="Facebook"
-              value={formik.values.facebook_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.facebook_url && formik.errors.facebook_url ? (
-              <div className="alert alert-danger">{formik.errors.facebook_url}</div>
-            ) : null}
-          </div>
-          <div className="form-group">
-            <label htmlFor="twitter_url">Twitter</label>
-            <input
-              type="text"
-              className="form-control"
-              id="twitter_url"
-              name="twitter_url"
-              placeholder="Twitter"
-              value={formik.values.twitter_url}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.twitter_url && formik.errors.twitter_url ? (
-              <div className="alert alert-danger">{formik.errors.twitter_url}</div>
-            ) : null}
-          </div> */}
-          <button type="submit" className="submit-btn">
-            Guardar
-          </button>
-        </form>
-      </div>
-    </div>
-                
-  ) 
+            <div className="form-group mb-4 mt-4">
+              <label htmlFor="long_description">Descripción Larga</label>
+              <input
+                type="text"
+                className="form-control"
+                id="long_description"
+                name="long_description"
+                placeholder="Descripción Larga"
+                value={formik.values.long_description}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.long_description && formik.errors.long_description &&
+                <div className="error-message message">{formik.errors.long_description}</div>
+              }
+            </div>
+            
+            <div  className="form-group mb-4 mt-4">
+              <label htmlFor="short_description">Descripción Corta</label>
+              <CKEditor
+                className="form-control"
+                editor={ClassicEditor }
+                data={initialValues.short_description}
+                onChange={(e,editor) => handleCKEditorChange(editor, formik.setFieldValue)}
+              />
+              {formik.touched.short_description && formik.errors.short_description &&
+                <div className="error-message message">{formik.errors.short_description}</div>
+              }
+            </div>
+
+            <div className="form-group mb-4 mt-4">
+              <label htmlFor="linkedin_url">Linkedin</label>
+              <input
+                type="text"
+                className="form-control"
+                id="linkedin_url"
+                name="linkedin_url"
+                placeholder="Linkedin"
+                value={formik.values.linkedin_url}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.linkedin_url && formik.errors.linkedin_url &&
+                <div className="error-message message">{formik.errors.linkedin_url}</div>
+              }
+            </div>
+
+            <div className="form-group mb-4 mt-4">
+              <label htmlFor="instagram_url">Instagram</label>
+              <input
+                type="text"
+                className="form-control"
+                id="instagram_url"
+                name="instagram_url"
+                placeholder="Instagram"
+                value={formik.values?.instagram_url}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}  
+              />
+              {formik.touched.instagram_url && formik.errors.instagram_url &&
+                <div className="error-message message">{formik.errors.instagram_url}</div>
+              }
+            </div>
+
+            <div className="form-group mb-4 mt-4">
+              <label htmlFor="facebook_url">Facebook</label>
+              <input
+                type="text"
+                className="form-control"
+                id="facebook_url"
+                name="facebook_url"
+                placeholder="Facebook"
+                value={formik.values.facebook_url}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.facebook_url && formik.errors.facebook_url &&
+                <div className="error-message message">{formik.errors.facebook_url}</div>
+              }
+            </div>
+
+            <div className="form-group mb-4 mt-4">
+              <label htmlFor="twitter_url">Twitter</label>
+              <input
+                type="text"
+                className="form-control"
+                id="twitter_url"
+                name="twitter_url"
+                placeholder="Twitter"
+                value={formik.values.twitter_url}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.twitter_url && formik.errors.twitter_url &&
+                <div className="error-message message">{formik.errors.twitter_url}</div>
+              }
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="logo">Logo</label>
+              <input
+                ref={formik.fileInput}
+                type="file"
+                className="form-control"
+                accept="image/png , image/jpeg"
+                id="logo"
+                name="logo"
+                placeholder="Logo"
+                onChange={(e) => {
+                  formik.setFieldValue('logo', e.currentTarget.files[0]);
+                }}
+              />
+              {formik.touched.logo && formik.errors.logo ? (
+                <div className="error-message message">{formik.errors.logo}</div>
+              ) : null}
+            </div>
+            {submitForm && <div className="success-message message">Organización editada correctamente</div>}
+            <button type="submit" className="submit-btn mb-4 mt-4">
+              Guardar
+            </button>
+            <Link className="submit-btn btn btn-danger" to={ORGANIZATION}>Cancelar</Link>
+
+          </form>
+        </div>
+      </div>            
+    ) 
   : <Redirect to={ORGANIZATION} />
-
-
 }
 export default OrganizationEdit;
--- src/Helpers/formValidations.js
+++ src/Helpers/formValidations.js
@@ -1,34 +1,101 @@
 import * as Yup from "yup";
 
-export const yupTitles = () =>
-  Yup.string()
-    .min(4, "Mínimo 4 caracteres")
-    .max(200, "Nombre muy largo")
-    .required("Campo obligatorio");
+import { 
+  DESCRIPTION_LONG,
+  DESCRIPTION_SHORT,
+  INPUT_REQUIRED,
+  INVALID_IMAGE_EXT,
+  INVALID_EMAIL,
+  PASSWORD_SHORT,
+  TITLE_LONG,
+  TITLE_SHORT,
+  URL_INVALID
+} from './messagesText'
 
-export const yupShortDesc = () =>
+const yupTitles = () =>
   Yup.string()
-    .min(10, "Mínimo 10 caracteres")
-    .max(250, "Descripción demasiado larga")
-    .required("Campo obligatorio");
+    .min(4, TITLE_SHORT)
+    .max(200, TITLE_LONG)
+    .required(INPUT_REQUIRED);
 
-export const yupLongDesc = () =>
+const yupShortDesc = () =>
   Yup.string()
-    .min(10, "Mínimo 10 caracteres")
-    .max(250, "Descripción demasiado larga")
-    .required("Campo obligatorio");
+    .min(10, DESCRIPTION_SHORT)
+    .max(250, DESCRIPTION_LONG)
+    .required(INPUT_REQUIRED);
 
-export const yupCustomString = (min, max, minMsg, maxMsg) =>
+const yupLongDesc = () =>
+  Yup.string()
+    .min(10, DESCRIPTION_SHORT)
+    .max(250, DESCRIPTION_LONG)
+    .required(INPUT_REQUIRED);
+
+const yupCustomString = (min, max, minMsg, maxMsg) =>
   Yup.string(min, max, minMsg, maxMsg)
     .min(min, minMsg)
     .max(max, maxMsg)
-    .required("Campo obligatorio");
+    .required(INPUT_REQUIRED);
 
-export const yupImages = () =>
-    Yup.mixed()
-      .required('Campo obligatorio')
-      .test("fileFormat", 'Archivo invalido', (value) =>
-        ["image/jpeg", "image/png"].includes(value?.type)
-      );
+const yupImages = () =>
+  Yup.mixed()
+    .required(INPUT_REQUIRED)
+    .test(
+      "fileFormat",
+      INVALID_IMAGE_EXT,
+      (value) => ["image/jpeg", "image/png"].includes(value?.type)
+    );
 
-export const yupUrlWebSite = () => Yup.string().url("Invalid url");
+const yupEmail = () =>   
+  Yup.string()
+    .email(INVALID_EMAIL)
+    .required(INPUT_REQUIRED)
+
+const yupUserRoles = () => 
+  Yup.string()
+    .required(INPUT_REQUIRED)
+
+const yupPassword = () =>
+  Yup.string()
+    .required(INPUT_REQUIRED)
+    .min(8, PASSWORD_SHORT)
+
+// RegisterForm validations 
+const yupFirstName = () =>
+  Yup.string()
+    .required(INPUT_REQUIRED)
+  
+const yupLastName = () =>
+  Yup.string()
+  .required(INPUT_REQUIRED)
+
+const yupPassRegister = (minMsg,passMsg) =>
+  Yup.string()
+    .min(6,minMsg )
+    .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,passMsg)
+    .required(INPUT_REQUIRED)
+
+const yupConfirmPass = (passRef,passMsg) =>
+  Yup.string()
+  .oneOf([Yup.ref(passRef), null], passMsg)
+  .required(INPUT_REQUIRED)
+
+const yupUrlWebSite = () => 
+  Yup.string()
+  .url(URL_INVALID)
+  .required(INPUT_REQUIRED)
+
+export {
+  yupCustomString,
+  yupConfirmPass,
+  yupEmail, 
+  yupFirstName,
+  yupImages, 
+  yupLongDesc, 
+  yupLastName,
+  yupPassword, 
+  yupPassRegister,
+  yupShortDesc, 
+  yupTitles, 
+  yupUserRoles,
+  yupUrlWebSite
+}
