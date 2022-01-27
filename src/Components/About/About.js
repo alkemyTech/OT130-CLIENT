b5a0f84f38c3ab566fe3bf9ebe9730158fb7773d
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-27 19:00:00 -0300

    creating component Text for dinamic texts and json for mock data

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Container, Image } from "react-bootstrap";
+
+
+import Title from '../Title/Title';
+import Texts from '../About/Texts';
+const data = require('./about.json');
 const About = () => {
-    //En la sección superior, mostrará el título "Nosotros", utilizando el componente para mostrar títulos.
+    
+    /* 
+    
+        Criterios de aceptacion: Se deberá crear una carpeta /About donde se agregaran todos los componentes referidos a esta página.
+        El componente principal deberá renderizarse al ingresar a la ruta /nosotros.
+        En la sección superior, mostrará el título "Nosotros", utilizando el componente para mostrar títulos.
+        Luego, mostrará el texto "Sobre Nosotros" de la ONG, que deberá poder recibir un texto a renderizar de forma dinámica, ya que se obtendrá de la API.
+    */
     return (
         <>
-        
+            <Title text="Nosotros" />
+            <Texts texts={data}/>
         </>
     )
 };
