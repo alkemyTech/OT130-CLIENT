50c37b8ff3c30038f9a1c6dad64ce19a3b41caef
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-27 16:58:17 -0300

    create folder About on components folder

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Image } from "react-bootstrap";
+const About = () => {
+    //En la sección superior, mostrará el título "Nosotros", utilizando el componente para mostrar títulos.
+    return (
+        <>
+        
+        </>
+    )
+};
+
+export default About;
