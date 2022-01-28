a29c1ad98f971518f3275559980e1d396405b134
Rodrigo <rodrigomendozar10@gmail.com>
2022-01-28 17:42:52 -0300

    app import order

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
@@ -13,13 +12,8 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import SlidesForm from './Components/Slides/SlidesForm';
 import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import { ScreenDashboard } from "./Screens/ScreenDashboard";
 import UserForm from './Components/Users/UsersForm';
 import './App.css';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -37,7 +31,6 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/backoffice" component={ScreenDashboard} />
         </Switch>
       </BrowserRouter>
     <div className="App">
