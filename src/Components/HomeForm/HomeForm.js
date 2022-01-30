65a270d89540db5dfccb486bfec64e8ba7971547
franco646 <franco@>
2022-01-30 20:58:31 -0300

    Update HomeForm.js

--- src/Components/HomeForm/HomeForm.js
+++ src/Components/HomeForm/HomeForm.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Alert, Button, Container, Spinner } from "react-bootstrap";
+
+import { getSlides } from "../../Services/slidesService";
+import { getOrganizationData } from "../../Services/organizationService";
 
 import WelcomeTextForm from "../WelcomeTextForm/WelcomeTextForm";
 import SlidesForm from "../Slides/SlidesForm";
 
 import { SLIDES_LIMIT } from "../../Helpers/constants";
-import { Get } from "../../Services/privateApiService";
 
 const HomeForm = () => {
   const [slides, setSlides] = useState([{}]);
@@ -15,15 +17,23 @@ const HomeForm = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const { data: slides } = await Get("slides");
-        const { data: organizationData } = await Get("organization");
-        setSlides(slides?.data);
-        setWelcomeText(organizationData?.data?.welcome_text);
-        setIsLoading(false);
-      } catch (error) {
-        setError(error);
-      }
+      const fetchSlidesData = async () => {
+        const { data: slides, error } = await getSlides();
+        if (error) {
+          return setError(error);
+        }
+        setSlides(slides);
+      };
+      const fetchOrganizationData = async () => {
+        const { data: organizationData, error } = await getOrganizationData();
+        if (error) {
+          return setError(error);
+        }
+        setWelcomeText(organizationData?.welcome_text);
+      };
+      await fetchSlidesData();
+      await fetchOrganizationData();
+      setIsLoading(false);
     };
     fetchData();
   }, []);
@@ -32,13 +42,19 @@ const HomeForm = () => {
     setSlides([...slides, {}]);
   };
 
-  return error ? (
-    <div className="text-center my-3">Algo salio mal :(</div>
-  ) : (
+  return (
     <Container>
       <h1 className="text-center my-3">Formulario de edición del home</h1>
-      {isLoading ? (
-        <div className="text-center">Cargando...</div>
+      {error ? (
+        <Alert variant="danger" className="text-center my-3">
+          Error: {error}
+        </Alert>
+      ) : isLoading ? (
+        <div className="text-center">
+          <Spinner animation="border" role="status" className="text-center">
+            <span className="visually-hidden">Cargando...</span>
+          </Spinner>
+        </div>
       ) : (
         <div>
           <h2 className="mt-3 fs-3">Editar texto de bienvenida</h2>
