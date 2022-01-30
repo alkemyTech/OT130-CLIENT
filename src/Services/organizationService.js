6be465b4ed092af88c66faae93c2ef3844ddf24c
franco646 <franco@>
2022-01-30 20:58:37 -0300

    Create organizationService.js

---
+++ src/Services/organizationService.js
@@ -0,0 +1,26 @@
+import { Get, Post } from "./privateApiService";
+
+const getOrganizationData = async () => {
+  try {
+    const { data } = await Get("organization");
+    return data;
+  } catch (error) {
+    if (error.response) {
+      return { error: error.response.data?.message };
+    }
+    return { error: error.message };
+  }
+};
+
+const updateOrganizationData = async (data) => {
+    try {
+        return await Post("organization", data)
+    } catch (error) {
+        if (error.response) {
+        return { error: error.response.data?.message };
+        }
+        return { error: error.message };
+    }
+}
+
+export { getOrganizationData, updateOrganizationData };
