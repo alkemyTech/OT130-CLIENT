c61aed8675a6fd6d8a2457c76127ff74e189af3c
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-31 12:53:22 -0300

    modifying the return of about service

--- src/Services/aboutService.js
+++ src/Services/aboutService.js
@@ -2,11 +2,11 @@ import dataAbout from '../Services/mocks/about.json';
 
 export const getData = async () => {
   const response = {
-    data: {},
-    error: ''
+    data: dataAbout,
+    error: null
   }
   try {
-    return response.data = dataAbout;
+    return response;
   } catch (error) {
     return response.error = 'Error al obtener los datos';
   }
