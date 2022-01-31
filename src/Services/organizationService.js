c02284fb72cdddd44a58434a80f032fd26252c27
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-31 19:20:50 -0300

    correction and create component for message when send form

--- src/Services/organizationService.js
+++ src/Services/organizationService.js
@@ -4,20 +4,20 @@ import { HOST } from "./config";
 export const getOrganizationData = async () => {
   const response = {
     data: {},
-    error: null,
+    error: '',
   }
   try {
     const { data } = await axios.get(`${HOST}/organization`);
     return response.data = data;
   } catch (error) {
-    return response.error = error;
+    return response.error = 'Error al obtener los datos de la organización';
   }
 };
 
 export const updateOrganizationData = async (body, id) => {
   const response = {
     data: {},
-    error:''
+    error: ''
   }
   try {
     const { data } = await axios.put(`${HOST}/organization/${id}`, body);
@@ -30,12 +30,12 @@ export const updateOrganizationData = async (body, id) => {
 export const getOrganizationDataById = async (id) => {
   const response = {
     data: {},
-    error: null
+    error: ''
   }
   try {
     const { data } = await axios.get(`${HOST}/organization/${id}`);
     return response.data = data;
   } catch (error) {
-    return  error;
+    return response.error = 'Error al obtener los datos de la organización';
   }
 };
