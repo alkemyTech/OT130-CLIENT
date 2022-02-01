e161df4e77c728cb4356fe5486841d4ea37079ff
SrNacho <xbynachox@gmail.com>
2022-02-01 11:33:20 -0300

    Removed return

--- src/Components/Organization/OrganizationEditForm.js
+++ src/Components/Organization/OrganizationEditForm.js
@@ -11,45 +11,43 @@ const OrganizationEditForm = ({
   handleChange,
   selectedImage,
   handleSelectImage,
-}) => {
-  return (
-    <form className="container" onSubmit={handleSubmit}>
-      <div className="organization-fields-container">
-        <div>
-          Logo:{' '}
-          <select
-            onChange={({ target: { value } }) => handleSelectImage(value, setFieldValue)}
-            name="logo"
-            defaultValue={selectedImage.image}
-          >
-            <option value={LOGO}>Logo 1</option>
-          </select>
-        </div>
-        <img src={selectedImage?.image} alt="logo" />
-        <input
-          className="input-field"
-          type="text"
-          name="name"
-          onChange={handleChange}
-          placeholder="Organization title"
-          value={values.name}
-        />
-        {touched.name && <ErrorMessage name="name" />}
-        <input
-          className="input-field"
-          type="text"
-          name="description"
-          onChange={handleChange}
-          placeholder="Organization short description"
-          value={values.description}
-        />
-        {touched.description && <ErrorMessage name="description" />}
-        <button className="submit-btn" type="submit">
-          Save
-        </button>
+}) => (
+  <form className="container" onSubmit={handleSubmit}>
+    <div className="organization-fields-container">
+      <div>
+        Logo:{' '}
+        <select
+          onChange={({ target: { value } }) => handleSelectImage(value, setFieldValue)}
+          name="logo"
+          defaultValue={selectedImage.image}
+        >
+          <option value={LOGO}>Logo 1</option>
+        </select>
       </div>
-    </form>
-  );
-};
+      <img src={selectedImage?.image} alt="logo" />
+      <input
+        className="input-field"
+        type="text"
+        name="name"
+        onChange={handleChange}
+        placeholder="Organization title"
+        value={values.name}
+      />
+      {touched.name && <ErrorMessage name="name" />}
+      <input
+        className="input-field"
+        type="text"
+        name="description"
+        onChange={handleChange}
+        placeholder="Organization short description"
+        value={values.description}
+      />
+      {touched.description && <ErrorMessage name="description" />}
+      <button className="submit-btn" type="submit">
+        Save
+      </button>
+    </div>
+  </form>
+);
 
 export default OrganizationEditForm;
