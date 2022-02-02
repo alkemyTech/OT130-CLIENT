1add9f32f9722289b1744ea8c0ed3dd457793bf4
SrNacho <xbynachox@gmail.com>
2022-02-02 18:19:28 -0300

    Added Alerts

---
+++ src/Components/Alert/index.js
@@ -0,0 +1,10 @@
+import Swal from 'sweetalert2';
+import { ALERT_OUT_TIMER } from '../../Helpers/constants';
+
+export const Alert = (title, message, icon) =>
+  Swal.fire({
+    icon: icon,
+    title: title,
+    text: message,
+    timer: ALERT_OUT_TIMER,
+  });
