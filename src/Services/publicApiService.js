233a571cf5071c05c1baf01f2943fe2c4e6d4616
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-02 16:09:55 -0300

    instance axios

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,15 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 import { API_URL } from '../config/api';
 
 const config = {
   baseURL: API_URL,
+  headers: {
+    Group: 130, //Aqui va el ID del equipo!!
+    'Content-type': 'application/json',
+  },
 };
+
+const instance = axios.create(config);
 
 const Get = () => {
   axios
-    .get("https://jsonplaceholder.typicode.com/users", config)
+    .get('https://jsonplaceholder.typicode.com/users', config)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
 
-export default Get;
+const Post = async (url, data) => await instance.post(url, data);
+
+
+export {
+  Get,
+  Post,
+};
