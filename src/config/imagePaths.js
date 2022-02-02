0e75856d8737c0189049999332d5286951015cf4
Kevin Diaz <kevindi0695@gmail.com>
2022-02-02 11:09:21 -0300

    organizacion de archivos

--- src/config/imagePaths.js
+++ src/config/imagePaths.js
@@ -1,10 +1,7 @@
 const DEFAULT_TITLE_BACKGROUND_IMAGE = "images/banner-img.jpg";
 const SUPPORTED_IMAGE_FORMATS = "image/png image/jpeg";
-const CATEGORY_URL = "/categories";
-
 
 export { 
     DEFAULT_TITLE_BACKGROUND_IMAGE,
-    SUPPORTED_IMAGE_FORMATS,
-    CATEGORY_URL
+    SUPPORTED_IMAGE_FORMATS,   
  };
