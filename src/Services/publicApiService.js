563743dcf02fa9a80cc148af9343de7104783ae5
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-02 17:03:46 -0300

    metodo peticion post.

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { API_URL } from '../config/api';
 
 const config = {
-  baseURL: API_URL,
+  baseURL: 'http://ongapi.alkemy.org/api',
   headers: {
     Group: 130, //Aqui va el ID del equipo!!
     'Content-type': 'application/json',
@@ -11,17 +10,9 @@ const config = {
 
 const instance = axios.create(config);
 
-const Get = () => {
-  axios
-    .get('https://jsonplaceholder.typicode.com/users', config)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-};
-
 const Post = async (url, data) => await instance.post(url, data);
 
 
 export {
-  Get,
   Post,
 };
--- src/config/api.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'http://ongapi.alkemy.org/api'; 
