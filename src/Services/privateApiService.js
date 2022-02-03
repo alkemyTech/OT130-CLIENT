b295b464fe44abe8cddcd15415fcca884124cdbb
IgnacioVivas <ignacab@live.com>
2022-02-02 21:28:39 -0300

    peticion patch reutilizable

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const token = localStorage.getItem("Token");
+
 const config = {
   baseURL: "http://ongapi.alkemy.org/api/",
   headers: {
+    Authorization: `Bearer ${token}`,
     Group: 130, //Aqui va el ID del equipo!!
     "content-type": "application/json",
   },
@@ -12,7 +15,7 @@ const instance = axios.create(config);
 
 const Post = async (url, data) => await instance.post(url, data);
 
-const Patch = async (url, data) => await instance.patch(url, data);
+const Patch = async (endpoint, body) => await instance.patch(config.baseURL + endpoint, body, config.headers);
 
 const Get = async (url) => await instance.get(url);
 
