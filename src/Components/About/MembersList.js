929e24d537edd866c7a6e70340b2258d5cca24ff
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-04 15:03:45 -0300

    arreglo isLoading para renderizar

--- src/Components/About/MembersList.js
+++ src/Components/About/MembersList.js
@@ -11,34 +11,35 @@ const MembersList = () => {
     <div>
       <h1 className="my-4">Listado De Miembros</h1>
       <ul className="row row-cols-2 row-cols-md-3 row-cols-lg-5 justify-content-around">
-        {allMembers.length > 0 ? (
-          allMembers.map((member) => {
-            return (
-              <div className="card-info m-1" key={member.id}>
-                <Card.Img
-                  variant="top"
-                  className=" col-12 col-lg-6 my-2 mx-auto"
-                  src={member.image}
-                />
-                <Card.Body className="p-0">
-                  <Card.Title className="text-center my-3">{member.name}</Card.Title>
-                  <Card.Text className="description-text">{member.description}</Card.Text>
-                  <div className="d-flex justify-content-around">
-                    <a href={member.facebookUrl} className="icon-social-media mx-2">
-                      <FaFacebook />
-                    </a>
-                    <a href={member.linkedinUrl} className="icon-social-media mx-2">
-                      <FaLinkedin />
-                    </a>
-                  </div>
-                </Card.Body>
-              </div>
-            );
-          })
-        ) : (
-          <p>No hay actividades</p>
-        )}
-         {isLoading && (
+        {!isLoading &&
+          (allMembers.length > 0 ? (
+            allMembers.map((member) => {
+              return (
+                <div className="card-info m-1" key={member.id}>
+                  <Card.Img
+                    variant="top"
+                    className=" col-12 col-lg-6 my-2 mx-auto"
+                    src={member.image}
+                  />
+                  <Card.Body className="p-0">
+                    <Card.Title className="text-center my-3">{member.name}</Card.Title>
+                    <Card.Text className="description-text">{member.description}</Card.Text>
+                    <div className="d-flex justify-content-around">
+                      <a href={member.facebookUrl} className="icon-social-media mx-2">
+                        <FaFacebook />
+                      </a>
+                      <a href={member.linkedinUrl} className="icon-social-media mx-2">
+                        <FaLinkedin />
+                      </a>
+                    </div>
+                  </Card.Body>
+                </div>
+              );
+            })
+          ) : (
+            <p>No hay actividades</p>
+          ))}
+        {isLoading && (
           <div className="position-absolute text-center">
             <Spinner variant="primary" animation="border" role="status" />
           </div>
