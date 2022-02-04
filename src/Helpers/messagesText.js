920532950d1261efb0486439850055ec5e90d3c8
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-04 14:31:34 -0300

    conflictos resueltos

--- src/Helpers/messagesText.js
+++ src/Helpers/messagesText.js
@@ -5,41 +5,6 @@ const UNKNOWN_ERROR = 'Ha ocurrido un error';
 
 // Mensajes de validación de formularios
 
-<<<<<<< HEAD
-const DESCRIPTION_LONG = "Descripción demasiado larga"
-const DESCRIPTION_SHORT = "Mínimo 10 caracteres"
-const EMAIL_TAKEN = 'El email ya ha sido registrado.'
-const INPUT_REQUIRED = "Campo obligatorio"
-const INVALID_EMAIL = "Email inválido"
-const INVALID_IMAGE_EXT = "Extensión inválida. Solo archivos jpeg o png"
-const TITLE_LONG = "Nombre muy largo"
-const TITLE_SHORT = "Mínimo 4 caracteres"
-const PASSWORD_SHORT = "Mínimo 6 caracteres"
-const PASSWORD_REGISTER_CONTAIN = 'La contraseña debe tener una longitud mínima de 6 caracteres, y contener al menos un número, una letra y un símbolo (por ejemplo: @#$%).'
-const PASSWORD_DONT_MATCH = "Las contraseñas no coinciden"
-const ALERT_ICON_SUCCESS = "success"
-const ALERT_ICON_ERROR = "error"
-const ALERT_ERROR_API = "Hubo un error en la aplicacíon"
-
-export {
-    DESCRIPTION_LONG, 
-    DESCRIPTION_SHORT, 
-    EMAIL_TAKEN,
-    INPUT_REQUIRED, 
-    INVALID_IMAGE_EXT, 
-    INVALID_EMAIL,
-    NETWORK_ERROR,
-    PASSWORD_SHORT,
-    PASSWORD_REGISTER_CONTAIN, 
-    PASSWORD_DONT_MATCH,
-    TITLE_LONG, 
-    TITLE_SHORT, 
-    UNKNOWN_ERROR, 
-    ALERT_ICON_SUCCESS,
-    ALERT_ICON_ERROR,
-    ALERT_ERROR_API
-}
-=======
 const DESCRIPTION_LONG = 'Descripción demasiado larga';
 const DESCRIPTION_SHORT = 'Mínimo 10 caracteres';
 const EMAIL_TAKEN = 'El email ya ha sido registrado.';
@@ -56,6 +21,7 @@ const PASSWORD_DONT_MATCH = 'Las contraseñas no coinciden';
 const SHOULD_BE_DIGITS_ONLY = 'Deben ser solo digitos';
 const ALERT_ICON_SUCCESS = 'success';
 const ALERT_ICON_ERROR = 'error';
+const ALERT_ERROR_API = "Hubo un error en la aplicacíon"
 
 
 export {
@@ -76,5 +42,5 @@ export {
   UNKNOWN_ERROR,
   ALERT_ICON_SUCCESS,
   ALERT_ICON_ERROR,
+  ALERT_ERROR_API
 };
->>>>>>> 5aba850346482cd6101cfb36364bc107c62682ef
