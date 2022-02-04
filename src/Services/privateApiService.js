735568b4e300a8b0252fa280498fc9b483192de1
Kevin Diaz <kevindi0695@gmail.com>
2022-02-04 17:31:32 -0300

    conflictos resueltos

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -5,14 +5,9 @@ const token = localStorage.getItem("Token");
 const config = {
   baseURL: 'http://ongapi.alkemy.org/api/',
   headers: {
-<<<<<<< HEAD
     Authorization: `Bearer ${token}`,
     Group: 130, //Aqui va el ID del equipo!!
     "content-type": "application/json",
-=======
-    //Group: 130, //Aqui va el ID del equipo!!
-    'content-type': 'application/json',
->>>>>>> 6ca2924b19e8ea5f22a25ed460ef53fd9926772b
   },
 };
 
@@ -29,30 +24,6 @@ const Post = async (url, body) => {
   return response;
 };
 
-const Patch = async (url, data) => await instance.patch(url, data);
-
-<<<<<<< HEAD
-const Patch = async ( url, data ) => await instance.patch( url, data );
-
-const Delete = async ( url, id ) => {
-  const response = { error: null, data: {} };
-  try {
-      const { data } = await instance.delete( `${ url }/${ id }` );
-      response.data = data;
-  } catch ( error ) {
-      response.error = error;
-  };
-  return response;
-};
- 
-const Get = async ( url ) => await instance.get( url );
-
-export {
-  Get,
-  Post,
-  Patch,
-  Delete
-=======
 const Get = async (url) => {
   const response = {};
   try {
@@ -73,7 +44,19 @@ const Put = async (url, body) => {
     response.error = error;
   }
   return response;
->>>>>>> 6ca2924b19e8ea5f22a25ed460ef53fd9926772b
 };
 
-export { Get, Post, Patch, Put };
+const Delete = async ( url, id ) => {
+  const response = {};
+  try {
+      const { data } = await instance.delete( `${ url }/${ id }` );
+      response.data = data;
+  } catch ( error ) {
+      response.error = error;
+  };
+  return response;
+};
+
+const Patch = async (url, data) => await instance.patch(url, data);
+
+export { Get, Post, Patch, Put, Delete};
