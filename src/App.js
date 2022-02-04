7311a2a214366976aa7ad08bb1a04be64bc5fd9c
Facundo Colavini <54385792+facundocolavini@users.noreply.github.com>
2022-02-04 17:59:26 -0300

    Ot130 34 formulario de actualizacion de datos de organizacion (#47)
    * adding forms edit in organization backoffice
    
    * adding inputs social media in form
    
    * fixing alert import to succes and error
    
    * the placeholder of the inputs was changed and error handling was added in the component

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import HomeForm from './Components/HomeForm/HomeForm';
 import CreateEditUser from './Components/Users/CreateEditUser';
+import OrganizationEdit from './Screens/Organization/OrganizationEdit';
 import { ScreenDashboard } from './Screens/ScreenDashboard';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -30,6 +32,7 @@ function App() {
           <Route path="/create-activity" component={ActivitiesCreation} />
           <Route path="/edit-activity/:id" component={ActivitiesEdition} />
           <Route path="/create-category" component={CategoriesForm} />
+          <Route path="/backoffice/organization/edit" component={OrganizationEdit} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/home" component={HomeForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
