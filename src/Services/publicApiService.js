f7087b6ec5b377f565467bbd3dd9b556b830349e
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-04 13:31:06 -0300

    try catch en publicService

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -10,8 +10,16 @@ const config = {
 
 const instance = axios.create(config);
 
-const Post = async (url, body) => await instance.post(url, body);
-
+const Post = async (url, body) => {
+  const response = {};
+  try {
+      const { data } = await instance.post(url, body);
+      response.data = data;
+  } catch (error) {
+      response.error = error;
+  }
+  return response;
+}
 
 export {
   Post,
