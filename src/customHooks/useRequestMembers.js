980d03dd0823b6a922375fff35f5080b248ecc6c
Rodrigo Mendoza <85702199+RodrigoM10@users.noreply.github.com>
2022-02-06 00:16:55 -0300

    Ot130 67 componente listado de miembros de org para página nosotros (#32)
    * Creacion de carpeta About y componente MembersList.js
    
    * vista y mock para memberList
    
    * membersService.js create
    
    * sweet alert install, customHooks y service para members creado
    
    * creacion de carpeta mocks y creacion de members.json
    
    * mock aplicado en membersService, borrado console.errorde useRequestMembers, reenderizado en memberList
    
    * resolucion de conflictos
    
    * conflictos resueltos
    
    * spinner add
    
    * agregado componente alert.
    
    * arreglo isLoading para renderizar
    
    * corrección de error de tipeo

---
+++ src/customHooks/useRequestMembers.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getMembers } from '../Services/membersService';
+import { ErrorAlert } from '../Components/Alert';
+import { ALERT_ERROR_API } from '../Helpers/messagesText';
+
+export function useRequestMembers() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async function () {
+        setIsLoading(true);
+        const { error, data } = await getMembers();
+        if (error) {
+          ErrorAlert(ALERT_ERROR_API)
+        } else {
+          setData(data);
+        }
+        setIsLoading(false);
+    })();
+  }, []);
+
+  return [data, isLoading];
+}
