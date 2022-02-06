f82b9dc420b370843c25ecaf01c6d248f503cfe0
Kevin Diaz <kevindi0695@gmail.com>
2022-02-06 16:45:08 -0300

    Renombre la constante del color del spinner

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DotLoader from "react-spinners/DotLoader";
-import { SPINNER_COLOR_GREEN_BLUE } from '../../config/colorConfig';
+import { GREEN_BLUE_COLOR } from '../../config/colorConfig';
 import "./Spinner.css";
 
 export const Spinner = () => {
@@ -8,7 +8,7 @@ export const Spinner = () => {
     <>
       <div className='spinner-container'>
         <DotLoader
-          color={SPINNER_COLOR_GREEN_BLUE}
+          color={GREEN_BLUE_COLOR}
           size={150}
         />
       </div>
