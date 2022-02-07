c0744dc4fec260fdd49aa928d6c2be063a442ad2
franco646 <franco@>
2022-02-07 16:20:27 -0300

    Update privateApiService.js

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -13,7 +13,16 @@ const config = {
 
 const instance = axios.create(config);
 
-const Post = async (url, body) => await instance.post(url, body);
+const Post = async (url, body) => {
+  const response = {};
+  try {
+      const { data } = await instance.post(url, body);
+      response.data = data;
+  } catch (error) {
+      response.error = error;
+  }
+  return response;
+}
 
 const Patch = async (url, data) => await instance.patch(url, data);
 
