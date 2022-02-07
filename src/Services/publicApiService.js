c93e13719caa93345210d97113bd7debd7413d10
Mateo <mateolewinzon@gmail.com>
2022-02-07 09:42:53 -0300

    fix

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const config = {
-  baseURL: "http://ongapi.alkemy.org/api/",
+  baseURL: 'http://ongapi.alkemy.org/api/',
   headers: {
     Group: 130, //Aqui va el ID del equipo!!
-    "content-type": "application/json",
+    'content-type': 'application/json',
   },
 };
 
 const Get = async (route, id) => {
   const response = { data: {}, error: null };
-  const url = route + `${id ? '/' + id : null}`;
+  const url = route + `${id ? '/' + id : ''}`;
 
   try {
     const { data } = await axios.get(url, config);
@@ -22,10 +22,9 @@ const Get = async (route, id) => {
   return response;
 };
 
-const baseURL = "http://ongapi.alkemy.org/api";
+const baseURL = 'http://ongapi.alkemy.org/api';
 const instance = axios.create(config);
 
 const getSlide = async () => await instance.get(baseURL + '/slides');
 
-export { Get, getSlide }
-
+export { Get, getSlide };
