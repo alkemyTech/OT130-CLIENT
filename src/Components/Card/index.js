64846ac48401bda852304bc7a72af84523a69a7d
IgAgg <xbynachox@gmail.com>
2022-02-08 16:52:00 -0300

    Removed log

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,21 +3,18 @@ import { IMAGE_NOT_AVAIBLE } from '../../Helpers/messagesText';
 import { EmptyScreen } from '../EmptyScreen';
 import './styles.css';
 
-const Card = ({ data }) => {
-  console.log(data);
-  return (
-    <div className="card">
-      {data.image ? (
-        <img className="card-img" src={data.image} alt="Card cap" />
-      ) : (
-        <div className="empty-image">{IMAGE_NOT_AVAIBLE}</div>
-      )}
-      <div className="card-body">
-        <h5 className="card-title">{data.name}</h5>
-        <p className="card-text">{data.description || data.content}</p>
-      </div>
+const Card = ({ data }) => (
+  <div className="card">
+    {data.image ? (
+      <img className="card-img" src={data.image} alt="Card cap" />
+    ) : (
+      <div className="empty-image">{IMAGE_NOT_AVAIBLE}</div>
+    )}
+    <div className="card-body">
+      <h5 className="card-title">{data.name}</h5>
+      <p className="card-text">{data.description || data.content}</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
