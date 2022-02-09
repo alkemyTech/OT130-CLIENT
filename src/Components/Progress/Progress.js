74ec5659c75bdde50b59d9d98d4699ecdcbfa867
Facundo Colavini <facu_colavini@hotmail.com>
2022-02-08 21:31:53 -0300

    removing spacing in the component

--- src/Components/Progress/Progress.js
+++ src/Components/Progress/Progress.js
@@ -3,7 +3,6 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 import './Progress.css';
 
 const Progress = ({ now }) => {
-
     return (
         <ProgressBar className='progress' now={now}/>
     );
