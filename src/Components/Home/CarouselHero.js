db88cc92a438ac71fafb54be2c32b324f4989ca2
Kevin Diaz <kevindi0695@gmail.com>
2022-02-09 20:52:29 -0300

    Manejo de error componentes en home

--- src/Components/Home/CarouselHero.js
+++ src/Components/Home/CarouselHero.js
@@ -18,32 +18,31 @@ function CarouselHero() {
     })()
   }, []);
 
-  return (
-    <>
-      <Carousel className='carousel'>
-        {getState && getState.map((res) =>
-          <Carousel.Item className='carousel-item' key={res.id}>
-            <img src={res.image} className='d-block w-100 img-carousel' alt="First slide"></img>
-          </Carousel.Item>)}
-      </Carousel>
-
-      <Container className='container-welcome-text'>
-        <Row>
-          <Col xs={12} className='welcome-text'>
-            <h2>TEXTO BIENVENIDA</h2>
-          </Col>
-          <Col xs={8} className='welcome-text'>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-              ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris."
-            </p>
-          </Col>
-        </Row>
-      </Container>
-
-    </>
-  )
-}
+    return (
+      <>
+        <Carousel className='carousel'>
+          {getState && getState.map((res) =>
+            <Carousel.Item className='carousel-item' key={res.id}>
+              <img src={res.image} className='d-block w-100 img-carousel' alt="First slide"></img>
+            </Carousel.Item>)}
+        </Carousel>
+  
+        <Container className='container-welcome-text'>
+          <Row>
+            <Col xs={12} className='welcome-text'>
+              <h2>TEXTO BIENVENIDA</h2>
+            </Col>
+            <Col xs={8} className='welcome-text'>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris."
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    )
+};
 
 export default CarouselHero;
--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -3,14 +3,19 @@ import CarouselHero from './CarouselHero';
 import News from './News';
 import './Hero.css';
 import './News.css';
+import { ErrorAlert } from '../Alert';
 
 function Home() {
-    return (
-        <>
-           <CarouselHero/>
-           <News/>
-        </>
-    )
-}
+    try {
+        return (
+            <>
+               <CarouselHero/>
+               <News/>         
+            </>
+        )
+    } catch ( error ) {
+        ErrorAlert( error );     
+    };  
+};
 
 export default Home;
