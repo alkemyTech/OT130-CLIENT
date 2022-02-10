2f0726eebe7d3ccbb40ad53b67f261656562fb40
franco646 <franco@>
2022-02-10 16:23:34 -0300

    Create slidesActions.js

---
+++ src/actions/slidesActions.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+import { getSlides as fetchSlides } from "../Services/slidesService";
+
+import {
+  getSlidesRequest,
+  getSlidesSuccess,
+  getSlidesFailure,
+} from "../reducers/slidesSlice";
+
+const getSlides = createAsyncThunk("slides/getSlides", async (_, thunkAPI) => {
+  thunkAPI.dispatch(getSlidesRequest());
+  const { data: slides, error } = await fetchSlides();
+  if (error) {
+    return thunkAPI.dispatch(getSlidesFailure(error));
+  }
+  thunkAPI.dispatch(getSlidesSuccess(slides));
+});
+
+export { getSlides };
+
