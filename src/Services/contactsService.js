ab84c4f0357a3c94feb6c3c8aea26ea3feb8bc18
Facundo Colavini <facu_colavini@hotmail.com>
2022-02-11 12:09:07 -0300

    Creating contact service

---
+++ src/Services/contactsService.js
@@ -0,0 +1,31 @@
+import { Post, Patch, Get, Delete } from './privateApiService';
+
+const  CONTACTS_ENDPOINT = process.env.REACT_APP_CONTACTS_ENDPOINT;
+
+const addContact = async (values) => {
+  return await Post(`${CONTACTS_ENDPOINT}`, values);
+};
+
+const updateContact = async (values, contact) => {
+  return await Patch(`${CONTACTS_ENDPOINT}/${contact.id}`, values);
+};
+
+const getContactById = async (id) => {
+  return await Get(`${CONTACTS_ENDPOINT}/${id}`);
+};
+
+const getContact = async () => {
+  return await Get(`${CONTACTS_ENDPOINT}`);
+};
+
+const deleteContact = async (id) => {
+  return await Delete(`${CONTACTS_ENDPOINT}/${id}`);
+};
+
+export { 
+    addContact,
+    deleteContact, 
+    getContact,
+    getContactById,
+    updateContact 
+};
