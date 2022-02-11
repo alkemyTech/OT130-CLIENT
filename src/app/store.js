3e89bb244d38a4687f1f223e4c7f0821ec477d0d
franco646 <franco@>
2022-02-11 17:06:40 -0300

    Elimina counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import slidesReducer from '../reducers/slidesSlice';
-import counterReducer from '../features/counter/counterSlice'
 
 const reducer = combineReducers({
   slides: slidesReducer,
-  counter: counterReducer
 })
 
 export default configureStore({
