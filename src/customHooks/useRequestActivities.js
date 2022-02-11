c59508a32681b9c2b69cf8e686cae9f4c8806208
Mateo <mateolewinzon@gmail.com>
2022-02-11 13:54:06 -0300

    added NetworkError case when receiving list of activities

--- src/customHooks/useRequestActivities.js
+++ src/customHooks/useRequestActivities.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { ErrorAlert } from '../Components/Alert';
-import { UNKNOWN_ERROR } from '../Helpers/messagesText';
+import { UNKNOWN_ERROR, NETWORK_ERROR } from '../Helpers/messagesText';
 import { getActivities } from '../Services/activitiesService';
 
 export function useRequestActivities() {
@@ -12,7 +12,7 @@ export function useRequestActivities() {
         setIsLoading(true);
         const { error, data } = await getActivities();
         if (error) {
-          ErrorAlert(UNKNOWN_ERROR)
+          ErrorAlert(error.message === 'Network Error' ? NETWORK_ERROR : UNKNOWN_ERROR)
         } else {
           setData(data);
         }
