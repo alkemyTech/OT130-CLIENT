1015db8c009196cc220b109c6aa90274eb00366e
Mateo <mateolewinzon@gmail.com>
2022-02-12 20:48:14 -0300

    pr fix

--- src/Services/activitiesService.js
+++ src/Services/activitiesService.js
@@ -1,9 +1,12 @@
 import { Post, Put, Get } from './privateApiService';
 
-export const getActivities = async () => await Get('activities');
+export const getActivities = async () => {
+  const { data, error } = await Get('activities');
+  return { data: data.data, error };
+};
 
 export const getActivityDataById = async (id) => await Get(`activities/${id}`);
 
 export const updateActivityDataById = async (id, body) => await Put(`activities/${id}`, body);
 
-export const saveActivityData = async (body) => await Post('activities',body)
+export const saveActivityData = async (body) => await Post('activities', body);
--- src/actions/activitiesActions.js
+++ src/actions/activitiesActions.js
@@ -3,7 +3,7 @@ import { getActivities } from '../Services/activitiesService';
 
 const fetchActivities = createAsyncThunk('activities/fetchActivities', async () => {
   const { error, data } = await getActivities();
-  return { error: error?.message, data: data?.data };
+  return { error: error?.message, data: data };
 });
 
 export { fetchActivities };
