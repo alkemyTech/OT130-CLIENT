60f75e967317a0679668737b714aee58412c690a
franco646 <franco@>
2022-02-12 20:36:46 -0300

    Update slidesActions.js

--- src/actions/slidesActions.js
+++ src/actions/slidesActions.js
@@ -1,21 +1,57 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { getSlides as fetchSlides } from "../Services/slidesService";
+import {
+  getSlides as getSlidesFromAPI,
+  deleteSlide as deleteSlideFromAPI,
+} from "../Services/slidesService";
+
+import {
+  ErrorAlert,
+  SuccessAlert,
+  showDeleteConfirmationAlert,
+} from "../Components/Alert";
 
 import {
   getSlidesRequest,
   getSlidesSuccess,
   getSlidesFailure,
+  deleteSlideRequest,
+  deleteSlideSuccess,
+  deleteSlideFailure,
 } from "../reducers/slidesSlice";
 
-const getSlides = createAsyncThunk("slides/getSlides", async (arg, { dispatch }) => {
-  dispatch(getSlidesRequest());
-  const { data: slides, error } = await fetchSlides();
-  if (error) {
-    return dispatch(getSlidesFailure(error));
+const getSlides = createAsyncThunk(
+  "slides/getSlides",
+  async (arg, { dispatch }) => {
+    dispatch(getSlidesRequest());
+    const { data, error } = await getSlidesFromAPI();
+    if (error) {
+      dispatch(getSlidesFailure(error));
+      return ErrorAlert("Error", error.message);
+    }
+    const slides = data?.data;
+    dispatch(getSlidesSuccess(slides));
   }
-  dispatch(getSlidesSuccess(slides));
-});
+);
 
-export { getSlides };
+const deleteSlide = createAsyncThunk(
+  "slides/deleteSlide",
+  async (slideId, { dispatch, getState }) => {
+    dispatch(deleteSlideRequest());
+    const { isConfirmed } = await showDeleteConfirmationAlert(async () => {
+      const { error } = await deleteSlideFromAPI(slideId);
+      if (error) {
+        dispatch(deleteSlideFailure(error));
+        return ErrorAlert("Error", error.message);
+      }
+    });
+    const { slides } = getState();
+    if (isConfirmed && !slides.error) {
+      dispatch(deleteSlideSuccess(slideId));
+      SuccessAlert("Eliminado!", "El slide fue eliminado");
+    }
+  }
+);
+
+export { getSlides, deleteSlide };
 
