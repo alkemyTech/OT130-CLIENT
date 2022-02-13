c5013cdee4c91dcbfb9830f34b34a997779304ab
Facundo Colavini <facu_colavini@hotmail.com>
2022-02-13 18:27:25 -0300

    create action users

---
+++ src/actions/usersActions.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getUsers, deleteUser } from '../Services/usersService';
+
+const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const { error, data } = await getUsers();
+  return { error: error?.message, data: data };
+});
+
+const deleteUsers = createAsyncThunk('users/deleteUsers', async (id) => {
+  const { error, data } = await deleteUser(id);
+  return { error: error?.message, data: data };
+});
+
+
+export { fetchUsers, deleteUsers };
+
+
