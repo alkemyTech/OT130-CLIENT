b2e776bf1b4116d385d40827cba5ed24abf1bfd1
Kevin Diaz <kevindi0695@gmail.com>
2022-02-13 10:31:45 -0300

    Correccion de index/home

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -3,19 +3,15 @@ import CarouselHero from './CarouselHero';
 import News from './News';
 import './Hero.css';
 import './News.css';
-import { ErrorAlert } from '../Alert';
 
 function Home() {
-    try {
+
         return (
             <>
                <CarouselHero/>
                <News/>         
             </>
-        )
-    } catch ( error ) {
-        ErrorAlert( error );     
-    };  
+        ) 
 };
 
 export default Home;
