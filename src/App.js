dd41ab6f9fc2cf3b40168bb8f143bd6e7f49ea91
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-14 11:13:34 -0300

    elimnacion de contador, limpieza y ordenado de codigo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Activities from './Components/Activities';
 import ActivitiesCreation from './Screens/Activities/ActivitiesCreation';
@@ -24,10 +23,9 @@ import CreateEditUser from './Components/Users/CreateEditUser';
 import OrganizationEdit from './Screens/Organization/OrganizationEdit';
 import { ScreenDashboard } from './Screens/ScreenDashboard';
 import Organization from './Screens/Organization/Organization';
+import BackoficeActivitiesList from './Components/Activities/BackoficeActivitiesList';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BackoficeActivitiesList from './Components/Activities/BackoficeActivitiesList';
-import UserAuth from './Components/Auth';
 
 
 function App() {
@@ -61,55 +59,6 @@ function App() {
           <Route path="/backoffice" component={ScreenDashboard} />
         </Switch>
       </BrowserRouter>
-
-      <div className="App">
-        <header className="App-header">
-          <UserAuth/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Counter />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <span>
-            <span>Learn </span>
-            <a
-              className="App-link"
-              href="https://reactjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redux
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux-toolkit.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Redux Toolkit
-            </a>
-            ,<span> and </span>
-            <a
-              className="App-link"
-              href="https://react-redux.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React Redux
-            </a>
-          </span>
-        </header>
-      </div>
     </>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import AuthReducer from '../reducers/auth/authReducer';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     AuthUser: AuthReducer, 
   },
 });
