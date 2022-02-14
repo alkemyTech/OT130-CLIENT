650dfac30f064af07c9b976f7fe29cba02c282d8
Mateo <mateolewinzon@gmail.com>
2022-02-14 15:32:05 -0300

    fix

--- src/config/transitions.js
+++ src/config/transitions.js
@@ -4,10 +4,14 @@ export const transitionsConfig = {
     atEnter: { opacity: 0 },
     mapStyles: (styles) => {
         return {
-          position: (styles.opacity === 1) ? undefined: 'absolute',
-          width: (styles.opacity === 1) ? undefined : '100%',
-          height: (styles.opacity === 1) ? undefined : '100%',
+          position: handleOpacityUnder100(styles,'absolute'),
+          width: handleOpacityUnder100(styles,'100%'),
+          height: handleOpacityUnder100(styles,'100%'),
           opacity: styles.opacity,
           overflow: 'hidden'
         }}
+}
+
+const handleOpacityUnder100 = (styles, cssValue) =>{
+  return (styles.opacity === 1) ? undefined : cssValue
 }
