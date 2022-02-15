9d33bf1240bf2af8bd7ea073a934200f86e86943
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-15 18:04:45 -0300

    market

--- src/Components/Contact/MapView.js
+++ src/Components/Contact/MapView.js
@@ -4,53 +4,41 @@ import 'leaflet/dist/leaflet.css';
 import './mapView.css';
 
 export const MapView = () => {
+  const positionONG = [-34.603465109247814, -58.375943679254284];
+  const animateRef = useRef(false);
 
-  const position = [-34.603465109247814, -58.375943679254284];
-  const animateRef = useRef(false)
-  
   function LocationMarker() {
-
-    const [position, setPosition] = useState(null)
+    const [position, setPosition] = useState(null);
     const map = useMapEvents({
       click() {
-        map.locate()
+        map.locate();
       },
       locationfound(e) {
-        setPosition(e.latlng)
-        map.flyTo(e.latlng, map.getZoom())
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
       },
-    })
-  
+    });
+
     return position === null ? null : (
       <Marker position={position}>
         <Popup>You are here</Popup>
       </Marker>
-    )
-  }
-  function SetViewOnClick({ animateRef }) {
-    const map = useMapEvent('click', (e) => {
-      map.setView(e.latlng, map.getZoom(), {
-        animate: animateRef.current || false,
-      })
-    })
-  
-    return null
+    );
   }
 
   return (
     <div className="d-flex justify-content-center my-5">
-      <MapContainer
-        className="leaflet-container"
-        center={position}
-        zoom={13}
-      >
+      <MapContainer className="leaflet-container" center={positionONG} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <LocationMarker />
-        <SetViewOnClick animateRef={animateRef} />
-
+        <Marker position={positionONG}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
