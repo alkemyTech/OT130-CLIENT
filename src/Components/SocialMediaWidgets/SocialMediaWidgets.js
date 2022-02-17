8814332f4712483ab2563c7d81425043834c88ad
franco646 <franco@>
2022-02-17 13:18:05 -0300

    Update SocialMediaWidgets.js

--- src/Components/SocialMediaWidgets/SocialMediaWidgets.js
+++ src/Components/SocialMediaWidgets/SocialMediaWidgets.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { TwitterTweet } from "react-social-plugins";
+import { TwitterTweet, LinkedinFollowCompany } from "react-social-plugins";
 
 const tweetsIdMock = [
   "1245758106392580097",
   "1440383594615042052",
   "1450535690199085058",
-  "1247299608570736641"
+  "1247299608570736641",
 ];
 
 const SocialMediaWidgets = () => {
   return (
     <div className="container mt-5">
-      {tweetsIdMock.map((tweetId) => (
-        <TwitterTweet
-          align="left"
-          coversation="none"
-          width={314}
-          tweetId={tweetId}
-          theme="light"
+      <div>
+        <LinkedinFollowCompany
+          companyId={68737437}
+          counter="top" // Or "right"
+          lang="en_US"
         />
-      ))}
+        {tweetsIdMock.map((tweetId) => (
+          <TwitterTweet
+            align="left"
+            coversation="none"
+            width={314}
+            tweetId={tweetId}
+            theme="light"
+          />
+        ))}
+      </div>
     </div>
   );
 };
