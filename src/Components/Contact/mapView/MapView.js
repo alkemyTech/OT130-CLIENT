737c0d7fdef48545388a447fdf5d58d58b5ab035
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-17 14:53:24 -0300

    map renderizado

--- src/Components/Contact/mapView/MapView.js
+++ src/Components/Contact/mapView/MapView.js
@@ -1,35 +1,56 @@
-import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer, Tooltip } from 'react-leaflet';
+import React, { useState } from 'react';
+import { MapContainer, Marker, TileLayer, Tooltip, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './mapView.css';
+import { LOGO } from '../../../assets';
+import { Button } from 'react-bootstrap';
 
-function GetIcon(_iconSize) {
+function GetIcon() {
   return L.icon({
     iconUrl: require('./iconMarker/icon_marker_location.png'),
-    iconAnchor: null,
+    iconRetinaUrl: require('./iconMarker/icon_marker_location.png'),
     iconSize: [50, 50],
+    iconAnchor: null,
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
   });
 }
+
 export const MapView = () => {
   const positionONG = [-34.603465109247814, -58.375943679254284];
 
-  return (
-    <div className="d-flex justify-content-center my-5">
-      <MapContainer center={positionONG} zoom={13}>
+
+  function LocationMarker() {
+    const [position, setPosition] = useState(positionONG);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound() {
+        setPosition(positionONG);
+        map.flyTo(positionONG, map.getZoom());
+      },
+    });
+    return position === null ? null : (
+      <Marker position={position} icon={GetIcon()}>
+        <Tooltip>
+          <img className="logo-somos-mas-map" src={LOGO} alt="LOGO-SOMOS-MAS" />
+        </Tooltip>
+      </Marker>
+    );
+  }
+
+  
+  return ( 
+    <div className="row map-div-container d-flex justify-content-center my-5 p-0">
+      <MapContainer className='col-12' center={positionONG} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={positionONG} icon={GetIcon()}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-          <Tooltip>Tooltip for Marker</Tooltip>
-        </Marker>
+        <LocationMarker />
       </MapContainer>
     </div>
   );
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,3 @@
 import logo from "./logo.png";
-import icon_location_map from "./icon_location_map.svg"
-
 export const LOGO = logo;
-export const ICON_LOCATION_MAP = icon_location_map;
+
