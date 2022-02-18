6b1e3a71d2f9063485e2a4ffdb4cc78d6a6a1e28
Mateo <mateolewinzon@gmail.com>
2022-02-18 10:35:30 -0300

    Helper improvement

--- src/Helpers/daysUntilDate.js
+++ src/Helpers/daysUntilDate.js
@@ -1,14 +1,14 @@
 export const daysToDate = (dueDate) => {
   const DATE_FORMAT = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
-  const dateElements = dueDate.split('-');
-  const year = dateElements[0];
-  const month = dateElements[1] - 1;
-  const day = dateElements[2];
 
   if (!DATE_FORMAT.test(dueDate))  {
     throw Error('Invalid date. Format is "yyyy-mm-dd".');
   }
 
+  const dateElements = dueDate.split('-');
+  const year = dateElements[0];
+  const month = dateElements[1] - 1;
+  const day = dateElements[2];
   const due = new Date(year, month, day);
   const milisecondsToDue = due.getTime() - new Date();
   const days = milisecondsToDue / 1000 / 60 / 60 / 24;
