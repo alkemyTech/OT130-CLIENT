6b2edc7c5937b2c140970333fd16cd44c2e0d109
Mateo <mateolewinzon@gmail.com>
2022-02-18 17:48:56 -0300

    Created responsive styles

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
-import Carousel from '../../Components/Carousel/Carousel';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './Content.css';
 import { daysToDue, countDown } from '../../Helpers/daysUntilDate';
 const Content = () => {
+  const date = '2022-12-03';
   return (
-    <Container>
+    <Container className='content-container' fluid>
       <Row>
-        <Carousel slides={''} />
+        <p className="subtitle-text">{date} Calle 123, Localidad, Provincia</p>
+      </Row>
+      <Row className='countdown'>
+        <p className="subtitle-text">Te quedan {countDown(date)} para participar.</p>
       </Row>
       <Row>
-        <h3></h3>
-      </Row>
-      <Row>
-        <h3>Te quedan </h3>
+        <Col>
+          <Image className="content-image" src="images/schools_content.png" />
+        </Col>
+        <Col lg="7">
+          <p className="campaign-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget tellus tellus. Sed
+            blandit, justo non sagittis luctus, justo urna venenatis orci, id pretium enim quam in
+            diam. Aenean scelerisque, felis a ultricies lacinia, erat risus tincidunt dolor, eget
+            finibus massa augue a purus. Sed convallis, sem eu accumsan dictum, nisl ligula finibus
+            justo, vel sodales magna nunc at tortor. Praesent dictum ullamcorper porta. Aenean non
+            imperdiet sem. Phasellus in tempor nulla, non lacinia massa. Ut vitae finibus ante.
+            Vivamus auctor efficitur erat, et maximus nisl faucibus mattis. Pellentesque finibus
+            elit a enim auctor lacinia.
+          </p>
+        </Col>
+        <Col>
+          <Image className="content-image" src="images/schools_content1.png" />
+        </Col>
       </Row>
     </Container>
   );
