5a1fe0d7918ede559c47cd3e09ae38c21393ec71
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2022-02-20 15:10:52 -0300

    OT130-112-Crear-reducer-de-miembros (#84)
    * create members reducer + action and add to store
    
    * delete BS spinner

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,14 @@
+import { membersReducer } from '../reducers/membersReducer';
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../reducers/auth/authReducer';
 import slidesReducer from '../reducers/slidesSlice';
-import activitiesReducer from '../reducers/activitiesReducer'
+import activitiesReducer from '../reducers/activitiesReducer';
 
 export default configureStore({
   reducer: {
-    slides: slidesReducer,
     activities: activitiesReducer,
-    authUser: authReducer, 
-  }
+    authUser: authReducer,
+    members: membersReducer,
+    slides: slidesReducer,
+  },
 });
-
