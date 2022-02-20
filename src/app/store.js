01263ba294591d2d6b209363952dd68d2aa4fd8d
Facundo Colavini <54385792+facundocolavini@users.noreply.github.com>
2022-02-20 15:28:11 -0300

    Ot130 111 crear reducer de usuarios (#82)
    * create action users
    
    * create reducers file for users
    
    * install redux dev tools
    
    * adding userReducer in the store
    
    * adding state handling with redux in the users component
    
    * eliminating unnecessary error condition when deleting user and space problems
    
    * change alert
    
    * uninstall redux-devtools-extension

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,16 @@
 import { membersReducer } from '../reducers/membersReducer';
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../reducers/auth/authReducer';
+import usersReducer from '../reducers/usersReducer';
 import slidesReducer from '../reducers/slidesSlice';
 import activitiesReducer from '../reducers/activitiesReducer';
+import authReducer from '../reducers/auth/authReducer';
 
 export default configureStore({
   reducer: {
     activities: activitiesReducer,
-    authUser: authReducer,
+    users: usersReducer,
+    authUser: authReducer, 
     members: membersReducer,
     slides: slidesReducer,
-  },
+  }
 });
