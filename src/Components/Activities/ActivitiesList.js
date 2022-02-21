e15654d3f73d663b35dacf06483ce574423a0484
Kevin Diaz <kevindi0695@gmail.com>
2022-02-21 10:40:24 -0300

    Agregue opciones de monto en el boton de mercadopago

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useRequestActivities } from '../../customHooks/useRequestActivities';
-import { Container, Spinner, Row, Card } from 'react-bootstrap';
+import { Container, Row, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchActivities } from '../../actions/activitiesActions';
 import { selectActivities } from '../../reducers/activitiesReducer';
@@ -37,7 +37,7 @@ const ActivitiesList = () => {
           })
         ) : isLoading ? (
           <div className="text-center">
-            <Spinner/>
+<Spinner/>
           </div>
         ) : (
           <p>{!error && 'No hay actividades'}</p>
--- src/Components/Donations/DonationButton.js
+++ src/Components/Donations/DonationButton.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { LinksMercadoPago } from "../../config/LinksMercadoPago";
 import { DONATION_BUTTON_TEXT } from "../../Helpers/messagesText";
 
 const MercadoPagoButton = () => {
   return (
-    <a className="btn btn-primary" href="https://mpago.la/25bic4G">
-      {DONATION_BUTTON_TEXT}
-    </a>
+    <div className="btn-group" role="group">
+      <button id="btnGroupDrop1" type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        {DONATION_BUTTON_TEXT}
+      </button>
+      <ul className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+        {LinksMercadoPago.map(link => (
+          <li>
+            <a className="dropdown-item" href={link.Link}>{link.Description}</a>
+          </li>
+        ))
+        }
+      </ul>
+    </div>
   );
 };
 
