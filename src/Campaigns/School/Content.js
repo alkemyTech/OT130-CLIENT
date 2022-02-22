b8acf5902c361500b827f7895fe1069fcafad77f
Mateo <mateolewinzon@gmail.com>
2022-02-22 17:05:35 -0300

    Added description text

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import './Content.css';
 import { daysToDue, countDown } from '../../Helpers/daysUntilDate';
+import { SCHOOL_CAMPAIGN_DATE, SCHOOL_CAMPAIGN_DESCRIPTION, SCHOOL_CAMPAIGN_LOCATION } from '../../Helpers/messagesText';
+
 const Content = () => {
-  const date = '2022-12-03';
+  const date = '2022-03-13';
   return (
     <Container className='content-container' fluid>
       <Row>
-        <p className="subtitle-text">{date} Calle 123, Localidad, Provincia</p>
+        <p className="subtitle-text">{SCHOOL_CAMPAIGN_DATE} en {SCHOOL_CAMPAIGN_LOCATION}</p>
       </Row>
       <Row className='countdown'>
         <p className="subtitle-text">Te quedan {countDown(date)} para participar.</p>
@@ -18,14 +20,7 @@ const Content = () => {
         </Col>
         <Col lg="7">
           <p className="campaign-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget tellus tellus. Sed
-            blandit, justo non sagittis luctus, justo urna venenatis orci, id pretium enim quam in
-            diam. Aenean scelerisque, felis a ultricies lacinia, erat risus tincidunt dolor, eget
-            finibus massa augue a purus. Sed convallis, sem eu accumsan dictum, nisl ligula finibus
-            justo, vel sodales magna nunc at tortor. Praesent dictum ullamcorper porta. Aenean non
-            imperdiet sem. Phasellus in tempor nulla, non lacinia massa. Ut vitae finibus ante.
-            Vivamus auctor efficitur erat, et maximus nisl faucibus mattis. Pellentesque finibus
-            elit a enim auctor lacinia.
+            {SCHOOL_CAMPAIGN_DESCRIPTION}
           </p>
         </Col>
         <Col>
