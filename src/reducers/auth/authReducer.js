94f9ffd9a62e6be6284106fbf4746dac728a4a26
Rodrigo <rodrigomendozar10@gmail.com>
2022-02-25 15:47:10 -0300

    constante creada USER_CURRENT_KEY

--- src/reducers/auth/authReducer.js
+++ src/reducers/auth/authReducer.js
@@ -1,16 +1,16 @@
 import { readFromLocalStorage, saveToLocalStorage } from '../../utils/localStorage';
-import { LOGIN_SUCCESS, LOGOUT_SUCCESS, LOCAL_STORAGE_AUTH_KEY } from './constants';
+import { LOGIN_SUCCESS, LOGOUT_SUCCESS, LOCAL_STORAGE_AUTH_KEY, USER_CURRENT_KEY } from './constants';
 
 const initialState = {
   isAuthenticated: readFromLocalStorage(LOCAL_STORAGE_AUTH_KEY) || false,
-  user: readFromLocalStorage('USER_CURRENT') || null,
+  user: readFromLocalStorage(USER_CURRENT_KEY) || null,
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
       saveToLocalStorage({ key: LOCAL_STORAGE_AUTH_KEY, value: true });
-      saveToLocalStorage({ key: 'USER_CURRENT', value: { user: action.payload } });
+      saveToLocalStorage({ key: USER_CURRENT_KEY, value: { user: action.payload } });
 
       return {
         ...state,
@@ -20,7 +20,7 @@ const authReducer = (state = initialState, action) => {
 
     case LOGOUT_SUCCESS:
       saveToLocalStorage({ key: LOCAL_STORAGE_AUTH_KEY, value: false });
-      saveToLocalStorage({ key: 'USER_CURRENT', value: { user: action.payload } });
+      saveToLocalStorage({ key: USER_CURRENT_KEY, value: { user: action.payload } });
 
       return {
         ...state,
