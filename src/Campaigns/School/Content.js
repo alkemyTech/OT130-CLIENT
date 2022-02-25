d6ec5cdf38aa3a17993926b385cac83bf0f0cf11
mateolewinzon <60374548+mateolewinzon@users.noreply.github.com>
2022-02-25 15:20:37 -0300

    OT130-132 estructurar componente contenido escolar (#108)
    * Created daysToDate helper
    
    * renamed countdown helper
    
    * Helper improvement
    
    * Helper improvement
    
    * Created component, improved helper
    
    * Created responsive styles
    
    * Added description text
    
    * PR fixes
    
    * PR fix
    
    * pr fixes

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './Content.css';
+import { countDown } from '../../Helpers/daysUntilDate';
+import {
+  SCHOOL_CAMPAIGN_DATE,
+  SCHOOL_CAMPAIGN_DESCRIPTION,
+  SCHOOL_CAMPAIGN_LOCATION,
+} from '../../Helpers/messagesText';
 
 const Content = () => {
+  const campaignEndDate = '2022-03-13';
   return (
-    <div>
-      
-    </div>
+    <Container className="content-container" fluid>
+      <Row>
+        <p className="subtitle-text">
+          {SCHOOL_CAMPAIGN_DATE} en {SCHOOL_CAMPAIGN_LOCATION}
+        </p>
+      </Row>
+      <Row className="countdown">
+        <p className="subtitle-text">Te quedan {countDown(campaignEndDate)} para participar.</p>
+      </Row>
+      <Row>
+        <Col>
+          <Image className="content-image" src="images/schools_content.png" />
+        </Col>
+        <Col lg="7">
+          <p className="campaign-description">{SCHOOL_CAMPAIGN_DESCRIPTION}</p>
+        </Col>
+        <Col>
+          <Image className="content-image" src="images/schools_content1.png" />
+        </Col>
+      </Row>
+    </Container>
   );
-}
- 
+};
+
 export default Content;
