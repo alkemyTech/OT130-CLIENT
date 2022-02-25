a106a9161be45ecc8889aa6148c97a85eab6ef78
IgnacioVivas <ignacab@live.com>
2022-02-25 11:04:54 -0300

    creando constantes de los diferentes roles de usuario

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,31 @@
-import { React } from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import { selectUserAuth } from '../../reducers/auth/authReducer'
+import { React } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { selectUserAuth } from '../../reducers/auth/authReducer';
 import { useSelector } from 'react-redux';
-import DonationButton from '../Donations/DonationButton'
+import DonationButton from '../Donations/DonationButton';
+import usuarioStandard from '../../Helpers/userTypes';
 
 function Header() {
 
-    const currentUser = useSelector(selectUserAuth);
-    const role = currentUser?.user?.role_id;
-    const standartUserRole = 2;
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        {
-                            role === standartUserRole ?
-                                <Nav.Link href="#donation"> <DonationButton /> </Nav.Link>
-                                :
-                                null
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  const currentUser = useSelector(selectUserAuth);
+  const role = currentUser?.user?.role_id;
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            {
+              (role === usuarioStandard) && <Nav.Link href="#donation"> <DonationButton /> </Nav.Link>
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
--- src/Helpers/userTypes.js
+++ src/Helpers/userTypes.js
@@ -1,2 +1,10 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { 2: "Usuario Standard", 1: "Usuario Adminisrador" };
+
+ const usuarioStandard = 2;
+ const usuarioAdministrador = 1;
+
+ export default{
+    usuarioStandard,
+    usuarioAdministrador,
+ };
