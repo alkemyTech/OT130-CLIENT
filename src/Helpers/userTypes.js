c67c1580872f8362f76b31b1770e93db1f1fe621
IgnacioVivas <ignacab@live.com>
2022-02-25 11:17:47 -0300

    resolviendo conflictos

--- src/Helpers/userTypes.js
+++ src/Helpers/userTypes.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { 2: "Usuario Standard", 1: "Usuario Adminisrador" };
-
+ const rolesDeUsuarios = { 2: "Usuario Standard", 1: "Usuario Adminisrador" };
  const usuarioStandard = 2;
  const usuarioAdministrador = 1;
 
  export default{
+    rolesDeUsuarios,
     usuarioStandard,
     usuarioAdministrador,
  };
