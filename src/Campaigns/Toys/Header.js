73b4eecf90d0c26c440a8cc47052be64d23f8f4e
Facundo Colavini <facu_colavini@hotmail.com>
2022-02-26 13:14:03 -0300

    create component header for campaigns toys

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { LOGOJUGUETES } from '../../assets';
+/* 
+Criterios de aceptacion: El componente debe estar diseñado para visualizarse primero para dispositivos moviles y contener breaking points para los diferentes tamaños de pantalla. El componente debe tener:
+
+Logo ONG, Logo campaña, Eslogan de campaña
+
+En dispositivos moviles móviles: solo va el logo campaña.
+
+En dispositivos tablet: va logo de campaña + logo de ONG
+
+Para pc de escritorio : va logo de campaña + logo de ONG + eslogan.
+
+Para smart tv: va logo de campaña + logo de ONG + eslogan. Se debe agregar color al fondo de header con transparencia.
+
+
+
+*/
+
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="header">
+      <Container>
+    
+      </Container>
+    </header>
   );
 }
  
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 import logo from "./logo.png";
+import logoJuguetes from "./logoJuguetes.png";
 export const LOGO = logo;
-
+export const LOGOJUGUETES = logoJuguetes;
