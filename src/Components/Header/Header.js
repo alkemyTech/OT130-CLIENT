2f33ab8ef84727eb2e03db278b072fc6060e582d
Shonakar <carlosjbordon@gmail.com>
2022-02-27 13:26:20 -0300

    Create Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const links = [
+  {
+    name: 'Inicio',
+    url: '/',
+  },
+  {
+    name: 'Nosotros',
+    url: '/',
+  },
+  {
+    name: 'Contacto',
+    url: '/contacts',
+  },
+  {
+    name: 'Campaña Escuelas',
+    url: '/school-campaign',
+  },
+  {
+    name: 'Campaña juguetes',
+    url: '/toys-campaign',
+  },
+];
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <img src="/src/assets/logo.png" alt="Img Logo" />
-        <a href="#">Enlace 1</a>
-        <a href="#">Enlace 2</a>
-        <a href="#">Enlace 3</a>
-        <a href="#">Enlace 4</a>
-        <a href="#">Enlace 5</a>
-        <a href="#">Enlace 6</a>
-      </header>
-    </div>
+    <header className=" d-flex justify-content-lg-between">
+      <div>
+        <img src="/src/assets/logo.webp" alt="Img Logo" />
+        {links.map(({ name, url }) => (
+          <a className=" mx-2 text-decoration-none " href={url}>
+            {name}
+          </a>
+        ))}
+      </div>
+      <div>
+        <Button className=" mx-2" variant="outline-primary">
+          Login
+        </Button>
+        <Button className=" mx-2">Registrarse</Button>
+      </div>
+    </header>
   );
 };
 
