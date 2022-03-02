e8fc0a27cacf0f3879d84091f8e36ece764952f7
Facundo Colavini <54385792+facundocolavini@users.noreply.github.com>
2022-03-02 08:53:33 -0300

    Ot130 141 proteccion de rutas de administrador (#120)
    * create protected route on backoffice paths
    
    * adding RequireAuth on Backoffice component
    
    * fix return

--- src/rutas/Backoffice.js
+++ src/rutas/Backoffice.js
@@ -11,22 +11,25 @@ import UserList from '../Components/Users/UserList';
 import CreateEditUser from '../Components/Users/CreateEditUser';
 import BackofficeActivitiesList from '../Components/Activities/BackofficeActivitiesList';
 import { ScreenDashboard } from '../Screens/ScreenDashboard';
+import { RequireAuthBackOffice } from '../Components/Auth/ProtectedBackOfficeRoute';
 
 function Backoffice() {
     return (
         <Switch>
-            <Route path="/backoffice/activities/create" component={ActivitiesCreation} />
-            <Route path="/backoffice/activities/edit/:id" component={ActivitiesEdition} />
-            <Route path="/backoffice/organization/edit" component={OrganizationEdit} />
-            <Route path="/backoffice/home" component={HomeForm} />
-            <Route path="/backoffice/create-slide" component={SlidesForm} />
-            <Route path="/backoffice/slides" component={SlidesList} />
-            <Route exact path="/backoffice/organization" component={Organization} />
-            <Route exact path="/backoffice/users" component={UserList} />
-            <Route exact path="/backoffice/users/create" component={CreateEditUser} />
-            <Route exact path="/backoffice/users/create/:id" component={CreateEditUser} />
-            <Route path="/backoffice/activities" component={BackofficeActivitiesList} />
-            <Route path="/" component={ScreenDashboard} />
+            <RequireAuthBackOffice>
+                <Route path="/backoffice/activities/create" component={ActivitiesCreation} />
+                <Route path="/backoffice/activities/edit/:id" component={ActivitiesEdition} />
+                <Route path="/backoffice/organization/edit" component={OrganizationEdit} />
+                <Route path="/backoffice/home" component={HomeForm} />
+                <Route path="/backoffice/create-slide" component={SlidesForm} />
+                <Route path="/backoffice/slides" component={SlidesList} />
+                <Route exact path="/backoffice/organization" component={Organization} />
+                <Route exact path="/backoffice/users" component={UserList} />
+                <Route exact path="/backoffice/users/create" component={CreateEditUser} />
+                <Route exact path="/backoffice/users/create/:id" component={CreateEditUser} />
+                <Route path="/backoffice/activities" component={BackofficeActivitiesList} />
+                <Route path="/" component={ScreenDashboard} />
+            </RequireAuthBackOffice>
         </Switch>
     )
 }
