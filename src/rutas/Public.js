aeb895aede8a26683b7decf2b245024b32ca97ed
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2022-03-02 18:28:50 -0300

    Ot130 124 crear componente header para web publica (#123)
    * header y mock
    
    * Create Header
    
    * add status
    
    * useBootstrap and activeLink
    
    * Fix logo and style
    
    * responsive
    
    * delete webp
    
    * soft reset
    
    * PR Fixes
    
    * PR Fixes

--- src/rutas/Public.js
+++ src/rutas/Public.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Activities from '../Components/Activities';
 import CategoriesForm from '../Components/Categories/CategoriesForm';
 import Contacts from '../Components/Contact';
@@ -14,26 +14,30 @@ import ToysCampaign from '../Campaigns/Toys/ToysCampaign';
 import Home from '../Components/Home/Index';
 import ActivityDetail from '../Components/Activities/Detail/ActivityDetail';
 import RegisterForm from '../Components/Auth/RegisterForm';
+import Header from '../Components/Header/Header';
 
 function Public() {
-    return (
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/activities" component={Activities} />
-            <Route path="/contacts" component={Contacts} />
-            <Route path="/create-category" component={CategoriesForm} />
-            <Route path="/create-news" component={NewsForm} />
-            <Route path="/create-testimonials" component={TestimonialForm} />
-            <Route path="/create-member" component={MembersForm} />
-            <Route path="/create-project" component={ProjectsForm} />
-            <Route path="/donate" component={Donation} />
-            <Route path="/school-campaign" component={SchoolCampaign} />
-            <Route path="/thanks" component={Thanks} />
-            <Route path="/toys-campaign" component={ToysCampaign} />
-            <Route path="/activities/:id" component={ActivityDetail} />
-            <Route path="/register" component={RegisterForm} />
-        </Switch>
-    )
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/activities" component={Activities} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/create-category" component={CategoriesForm} />
+        <Route path="/create-news" component={NewsForm} />
+        <Route path="/create-testimonials" component={TestimonialForm} />
+        <Route path="/create-member" component={MembersForm} />
+        <Route path="/create-project" component={ProjectsForm} />
+        <Route path="/donate" component={Donation} />
+        <Route path="/school-campaign" component={SchoolCampaign} />
+        <Route path="/thanks" component={Thanks} />
+        <Route path="/toys-campaign" component={ToysCampaign} />
+        <Route path="/activities/:id" component={ActivityDetail} />
+        <Route path="/register" component={RegisterForm} />
+      </Switch>
+    </>
+  );
 }
 
-export default Public
+export default Public;
