1b8c98927a774a76f9febeb34a0e5e11a9d9ddfa
franco646 <franco@>
2022-03-02 10:03:45 -0300

    Create News.js

---
+++ src/Components/News/News.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+import NewsSearchForm from '../NewsSearchForm/NewsSearchForm';
+
+const News = () => {
+    return (
+        <Container>
+            <NewsSearchForm />
+        </Container>
+    )
+}
+
+export default News;
