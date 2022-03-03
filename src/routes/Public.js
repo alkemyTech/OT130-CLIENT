5bb0e50541662d7afd56833f477c0e32b39fa80f
mateolewinzon <60374548+mateolewinzon@users.noreply.github.com>
2022-03-03 11:18:54 -0300

    OT130-128 Integrar header y footer en web pública (#90)
    * Added Layout component
    
    * Pr fixes
    
    * Deleted mock files

--- src/routes/Public.js
+++ src/routes/Public.js
@@ -14,13 +14,12 @@ import ToysCampaign from '../Campaigns/Toys/ToysCampaign';
 import Home from '../Components/Home/Index';
 import ActivityDetail from '../Components/Activities/Detail/ActivityDetail';
 import RegisterForm from '../Components/Auth/RegisterForm';
-import Header from '../Components/Header/Header';
+import Layout from '../Components/Layout/Layout'
 import { PublicRoute } from '../router/PublicRoute';
 
 function Public() {
   return (
-    <>
-      <Header />
+    <Layout>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/activities" component={Activities} />
@@ -37,7 +36,7 @@ function Public() {
         <Route path="/activities/:id" component={ActivityDetail} />
         <PublicRoute path="/register" component={RegisterForm} />
       </Switch>
-    </>
+    </Layout>
   );
 }
 
