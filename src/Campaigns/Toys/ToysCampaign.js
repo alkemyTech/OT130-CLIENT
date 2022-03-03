abaff2ae04e5d30be454228293676ef6ca67bee2
IgnacioVivas <ignacab@live.com>
2022-03-03 10:15:05 -0300

    resolviendo conflictos

--- src/Campaigns/Toys/ToysCampaign.js
+++ src/Campaigns/Toys/ToysCampaign.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import HeaderCampaings from '../Shared/HeaderCampaings';
-=======
-import HeaderCampaings  from '../Shared/HeaderCampaigns';
->>>>>>> c36837974a11778952173e8515d2983988d472d9
 import Slider from './Slider';
 import Content from './Content/Content';
 import Footer from './Footer';
@@ -11,11 +7,7 @@ import Footer from './Footer';
 const ToysCampaign = () => {
   return (
     <>
-<<<<<<< HEAD
-      <HeaderCampaings campaing={"toys"} />
-=======
       <HeaderCampaings campaign={"toys"} />
->>>>>>> c36837974a11778952173e8515d2983988d472d9
       <Slider />
       <Content />
       <Footer />
