d010007107df29fd4839496ec261c2bdc189676b
Mateo <mateolewinzon@gmail.com>
2022-03-03 10:52:43 -0300

    Deleted mock files

--- src/Components/Layout/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <p style={{ fontSize: '40px' }}>TODO footer</p>;
-  // TODO footer web publica
-};
-
-export default Footer;
--- src/Components/Layout/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <p style={{ fontSize: '40px' }}>TODO header</p>;
-  // TODO header web publica
-};
-
-export default Header;
