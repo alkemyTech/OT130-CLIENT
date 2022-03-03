1c1f05cdded7c5a8aa28064d3e00a2bb0a71c241
Shonakar <carlosjbordon@gmail.com>
2022-03-03 15:09:59 -0300

    slider

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,22 +1,18 @@
-import React from "react";
-import { Carousel as BootstrapCarousel } from "react-bootstrap";
+import React from 'react';
+import { Carousel as BootstrapCarousel } from 'react-bootstrap';
 
-import { CAROUSEL_INTERVAL } from "../../Helpers/constants";
+import { CAROUSEL_INTERVAL } from '../../Helpers/constants';
 
-import "./Carousel.css";
+import './Carousel.css';
 
 const Carousel = ({ slides }) => (
-  <BootstrapCarousel>
+  <BootstrapCarousel fluid>
     {slides.map((slide, index) => (
-      <BootstrapCarousel.Item key={index} interval={CAROUSEL_INTERVAL}>
-        <img
-          className="slide-image d-block w-100"
-          src={slide.image}
-          alt={slide.name}
-        />
+      <BootstrapCarousel.Item key={index} interval={CAROUSEL_INTERVAL} controls={false}>
+        <img className="slide-image d-block w-100" src={slide.image} alt={slide.name} />
         <BootstrapCarousel.Caption>
-          <h3>{slide.name}</h3>
-          <p>{slide.description}</p>
+          <h3 className="slide-name">{slide.name}</h3>
+          <p className="slide-description">{slide.description}</p>
         </BootstrapCarousel.Caption>
       </BootstrapCarousel.Item>
     ))}
