4aed85beb1c5dc521f48d852b7a629f4c6a3482e
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2022-03-04 15:28:08 -0300

    0 t130 issue#144 texto de bienvenida de la web publica y Imagenes en el slide de escuela. (#166)
    * Add drive img school carousel
    
    * welcome text
    
    * import font and syile text

--- src/Components/Home/CarouselHero.js
+++ src/Components/Home/CarouselHero.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getSlides } from "../../Services/homeService";
+import { getSlides } from '../../Services/homeService';
 import { Carousel, Container, Row, Col } from 'react-bootstrap';
 import { ErrorAlert } from '../Alert';
 import { Spinner } from '../Spinner/Spinner';
 
 function CarouselHero() {
-
   const [getState, setGetState] = useState();
   const [loading, setLoading] = useState(false);
 
@@ -24,29 +23,33 @@ function CarouselHero() {
 
   return (
     <>
-      {loading && <Spinner/>}      
-      <Carousel className='carousel'>
-        {getState && getState.map((res) =>
-          <Carousel.Item className='carousel-item' key={res.id}>
-            <img src={res.image} className='d-block w-100 img-carousel' alt="First slide"></img>
-          </Carousel.Item>)}
+      {loading && <Spinner />}
+      <Carousel className="carousel">
+        {getState &&
+          getState.map((res) => (
+            <Carousel.Item className="carousel-item" key={res.id}>
+              <img src={res.image} className="d-block w-100 img-carousel" alt="First slide"></img>
+            </Carousel.Item>
+          ))}
       </Carousel>
-      <Container className='container-welcome-text'>
+      <Container className="container-welcome-text">
         <Row>
-          <Col xs={12} className='welcome-text'>
-            <h2>TEXTO BIENVENIDA</h2>
+          <Col xs={12} className="welcome-text">
+            <h2>Somos Más</h2>
           </Col>
-          <Col xs={8} className='welcome-text'>
+          <Col xs={8} className="welcome-text">
             <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-              ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris."
+              Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y
+              vecinos del barrio La Cava generando procesos de crecimiento y de inserción social.
+              Buscamos mejorar la calidad de vida de niños y familias en situación de vulnerabilidad
+              en el barrio, otorgando un cambio de rumbo en cada individuo a través de la educación,
+              salud, trabajo, deporte, responsabilidad y compromiso.
             </p>
           </Col>
         </Row>
       </Container>
     </>
-  )
+  );
 }
 
 export default CarouselHero;
