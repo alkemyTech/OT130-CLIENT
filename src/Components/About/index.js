f4e14fa02ac8e26b2e4cb7425d65912dcb53b7d1
Rodrigo Mendoza <85702199+RodrigoM10@users.noreply.github.com>
2022-03-04 16:48:41 -0300

    organizacion ed layout vista (#169)
    * organizacion ed layout vista
    
    * agregado lista de miembros

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ListTexts from '../ListTexts/ListTexts';
 import Title from '../Title/Title';
 import SocialMediaWidgets from '../SocialMediaWidgets/SocialMediaWidgets';
+import MembersList from './MembersList';
 import { getData } from '../../Services/aboutService';
 import { Spinner } from '../Spinner/Spinner';
 import { ErrorAlert } from '../Alert';
@@ -29,11 +30,16 @@ const About = () => {
     <>
       {loading && <Spinner />}
       {dataTexts &&
-        <>
+        <div>
           <Title text={dataTexts[0].title} />
           <ListTexts itemSection={dataTexts} />
+          <div >
           <SocialMediaWidgets />
-        </>
+          </div>
+          <div>
+            <MembersList />
+          </div>
+        </div>
       }
     </>
   )
--- src/Components/SocialMediaWidgets/SocialMediaWidgets.js
+++ src/Components/SocialMediaWidgets/SocialMediaWidgets.js
@@ -4,32 +4,36 @@ import {
   LINKEDIN_COMPANY_ID,
   TWITTER_WIDGET_WIDTH,
 } from "../../Helpers/constants";
+import './socialMediaWidget.css'
 
 const tweetsIdMock = [
   "1245758106392580097",
   "1440383594615042052",
   "1450535690199085058",
-  "1247299608570736641",
+  "1247299608570736641"
 ];
 
 const SocialMediaWidgets = () => {
   return (
-    <div className="container mt-5">
-      <div>
+    <div className="my-5 ">
+      <div className="d-flex justify-content-center aling-items-center">
         <LinkedinFollowCompany
           companyId={LINKEDIN_COMPANY_ID}
           counter="top"
           lang="en_US"
         />
+        <div className="row aling-items-center justify-content-center social-media-widget">
         {tweetsIdMock.map((tweetId) => (
           <TwitterTweet
+            className="m-2"
             align="left"
             coversation="none"
             width={TWITTER_WIDGET_WIDTH}
             tweetId={tweetId}
             theme="light"
           />
-        ))}
+          ))}
+          </div>
       </div>
     </div>
   );
